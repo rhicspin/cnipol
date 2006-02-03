9568df320a6dafb924b40fac932936df12eebb74
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-03 00:00:42 +0000

    header files. Asym.h:    declare globals.
    AsymMain.h
       initialize scopes with default values
    
    rhicpol.h
      define datatypes
    
    rpoldata.h
      data structure
    
    WeightedMean.h:
       header file for WeightedMean routine.
    
    
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@7 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ src/Asym.h
@@ -0,0 +1,288 @@
+// Header file for Asym
+// Author   : Itaru Nakagawa
+// Creation : 11/18/2005         
+
+#ifndef ASYM_STREAM_H
+#define ASYM_STREAM_H
+#include <string.h>       // this is funny, but need to be included here to 
+using namespace std;      //declare string in structure
+
+#ifdef NULL
+#undef NULL
+#endif
+#define NULL 0
+
+#define NWORDS_PAWC 20000000      // paw memory size 
+#define RECLEN 1024
+#define HMINENE 100.              // minimum energy for histograms 
+#define HMAXENE 1500.             // maximum energy for histograms
+#define HENEBIN 180               // number of energy bin in banana plot
+#define NTLIMIT 100000000
+#define RAMPTIME 350              // duration of the ramp measurement (sec)
+#define MAXDELIM 350              // maximum number of delimiter
+#define NTBIN 21                  // number of -t bin
+
+#define NSTRIP 72                 // Number of strip channels
+#define G2k 1e6                   // GeV -> keV
+#define k2G 1e-6                  // keV -> GeV
+#define MASS_12C 11.187e6         // Mass Carbon in [keV]
+#define C_CMNS 29.98              // Speed of Light in [cm/ns]
+
+
+
+// whole info for one event
+typedef struct {
+    int stN;   // strip number
+    int amp;   // amplitude of signal
+    int tdc;   // time of flight
+    int tdcmax;  // tdc max amplitude
+    int intg;  // integral of signal
+    int bid;   // bunch crossing number
+    long delim;
+    int rev0;
+    int rev;
+} processEvent;
+
+typedef struct {
+    float phys;         // physics asymmetry
+    float physE;        // physics asymmetry error
+    float acpt;         // acceptance asymmetry
+    float acptE;        // acceptance asymmetry error
+    float lumi;         // luminosity asymmetry
+    float lumiE;        // luminosity asymmetry error
+} asymStruct;
+
+typedef struct {
+    // Constraint parameter for Data processing 
+    int enel; // lower kinetic energy threshold (keV) 
+    int eneu; // upper kinetic energy threshold (keV)
+    int widthl; // lower banana cut (ns)
+    int widthu; // upper banana cut (ns)
+    int FEEDBACKMODE; // fit 12C peak first and feedback tshift and sigma
+    int CMODE; // Calibration mode 0:off 1:on
+    int DMODE; // dead layer study mode 0:off 1:on
+    int TMODE; // T0 study mode 0:off 1:on
+    int AMODE; // A0,A1 study (signal Amp vs. Int) mode 0:off 1:on
+    int BMODE; // create banana curve (E-T) plots 0:off 1:on
+    int ZMODE; // with/out T0 subtraction 0:with 1:without
+    int MESSAGE; // message mode 1: exit just after run begin 
+    int CBANANA; // constant width banana cut :1, <sigma> Mass Cut :2
+    int UPDATE; // 1: keep update of the histogram
+    int MMODE;   // mass mode 
+    int NTMODE;  // if 1 store NTUPLEv
+    int RECONFMODE; // if 1 reconfigure from file 
+    int RAMPMODE;    // if 1 prepare the histograms for ramp measurement
+    int STUDYMODE;    // if 1 study mode
+  float MassSigma; // banana curve cut within <MassSigma> away from the 12C mass
+  float OneSigma; // 1-sigma of 12C mass distribution in [keV]
+  int tshift; // time shift
+  float WCMRANGE; // Wall Current Monitor process Fill range
+  float MassLimit; //Lower Mass limit for peak position adjustment fit
+  int thinout; // Every <thinout> event to be feed into feedback routine
+} datprocStruct; 
+
+typedef struct {
+    long ia;
+    long it;
+    long is;
+    long ib;
+    long id;
+    int strip;
+    float e;
+    float tof;
+    int spin;
+} atdata_struct;
+
+typedef struct {
+  float Ct;    // TDC Count unit (ns/channel)
+  float E2T;   // kinetic energy -> tof
+  float M2T;   // Mass->tof
+  float T2M;   // ToF -> Mass
+} StructRunConst;
+
+
+typedef struct {
+  float TshiftAve;
+} StructAnalysis;
+
+
+typedef struct {
+  int StartTime;
+  int StopTime;
+  int RunTime;
+  float EvntRate;
+  float ReadRate;
+  float WcmAve;
+  double BeamEnergy;
+  int RHICBeam;
+} StructRunInfo;
+
+typedef struct {
+  double RunID;
+  string config_file_s;
+  string masscut_s;
+  string tshift_s;
+}StructRunDB ;
+
+
+typedef struct {
+  int CONFIG;
+  int MASSCUT;
+  int TSHIFT;
+}StructExtInput;
+
+
+typedef struct {
+  float total;
+  float average;
+  int counter;
+} StructAverage;
+
+typedef struct {
+  float mean;
+  float RMS;
+  float RMSnorm;
+  float maxbin;
+} StructHistStat;
+
+typedef struct {
+  float mdev[72];
+  float RMS[72];
+} StructFeedBack;
+
+
+typedef struct {
+  int RECBEGIN;
+  int PCTARGET;
+  int WCMADO;
+  int BEAMADO;
+  int RHICCONF;
+} StructReadFlag ;
+
+
+typedef struct {
+  int feedback;
+} StructFlag;
+
+
+
+
+
+
+
+
+
+extern "C" {
+
+  void hhbook1d_(int*, char*, int*, float*, float*,  int);
+  void hhbook2d_(int*, char*, 
+		 int*, float*, float*, int*, float*, float*,  int);
+  void hhf1_(int*, float*, float*);
+  void hhf2_(int*, float*, float*, float*);
+  void hhlimit_(int*);
+  void hhropen_(int*, char*, char*, char*, int*, int*,  int,int,int);
+  void hhrend_(char*, int);
+  void hhrout_(int*, int*, char*, int);
+  void hhrammar_(float*, int*);
+  void hhrebin_(int*, float*, float*, float*, float*, int*, int*, int*);
+  void hhpak_(int*, float*);
+  void hhpake_(int*, float*);
+  float hhmax_(int*);
+  float hhstati_(int*, int*, char*, int*, int);
+  //  void hhkind_(int*, int*, char*, int);
+  void hfithn_(int*, char*, char*, int*, float*, float*, float*, float*, float*, float*, int, int);
+}
+
+
+int readloop(void);
+
+int readdb(double RUNID);
+int hist_book(char *);
+int hist_close(char *);
+int read_rebin(char *);
+int read_ramptiming(char *);
+int Initialization();
+int spill_end(recordConfigRhicStruct *);
+int sqass(float, float, float, float, float *, float *);
+int event_process(processEvent *, recordConfigRhicStruct *);
+int end_process(recordConfigRhicStruct *);
+
+int printConfig(recordConfigRhicStruct *);
+void reConfig(recordConfigRhicStruct *);
+void calcRunConst(recordConfigRhicStruct *);
+float ekin(float, float);
+int ExclusionList(int i, int j, int RHICBeam);
+int calcAsymmetry(int a, int b, int atot, int btot, float &Asym, float &dAsym);
+int DisabledDet(int det);
+int FlipSpinPattern(int mode);
+void SpecificLuminosity(float&, float&, float&);
+float TshiftFinder(int);
+int BunchSelect(int);
+
+// GLOBAL VARIABLES
+
+extern int spinpat[120]; // spin pattern 120 bunches
+extern int fillpat[120]; // fill pattern 120 bunches
+extern int wcmfillpat[120]; //  fill pattern within the Wall Current Monitor Average Ragne 
+extern float wcmdist[120];  // wall current monitor 120 bunches
+
+extern long int Ncounts[6][120]; // counts 6detectors 120 bunches
+extern long int NTcounts[6][120][NTBIN];  // counts 6detectors 120 bunches 6 tranges
+extern long int NDcounts[6][120][MAXDELIM];  // counts 6detectors 120 bunches per delimiter
+extern long int NStrip[3][72]; // counts 72 strips 3 spin states
+ 
+extern long int NRcounts[6][120][RAMPTIME];// counts 6det 120bunch RAMPTIME sec
+
+extern char * confdir;
+extern char datafile[256];   // data file name 
+extern char reConfFile[256];           // update configuration file for T0 info
+extern char conf_file[256];           // update configuration file for T0 info
+
+extern float ramptshift[500]; // ramp timing shift 
+
+extern long int Nevcut;   // number of events after 1st cut (whole data)
+extern long int Nevtot;   // number of total events (whole data) 
+extern long int Nread;    // real total events (completely everything)
+extern long int Nwcmtot;  // number of total wall current monitor data.
+extern int Nskip;         // number of events to be skipped in data process 
+extern int NFilledBunch;  // number of filled bunch
+
+extern long int Ngood[120];   // number of evts after carbon cut 
+extern long int Ntotal[120];  // number of evts before carbon cut 
+extern long int Nback[120];   // number of evts below the curbon cut
+extern datprocStruct dproc;
+extern atdata_struct atdata;
+extern StructRunDB rundb;
+extern StructRunInfo runinfo;
+extern StructExtInput extinput;
+extern StructAverage average;
+extern StructHistStat hstat;
+extern StructFeedBack feedback;
+extern StructRunConst runconst;
+extern StructFlag Flag;
+extern StructReadFlag ReadFlag;
+extern StructAnalysis analysis;
+
+extern float phiRun5[72];   // phi-angle for each strips of Run5 (l=18.5cm)
+extern float phiRun5t[72];  // phi-angle for each strips in approximation 45,90,135... 
+
+
+// target position infomation 
+extern int ndelim ;
+extern long int VtgtLinear[MAXDELIM]; 
+extern long int VtgtRotary[MAXDELIM];
+extern long int HtgtLinear[MAXDELIM];
+extern long int HtgtRotary[MAXDELIM];
+extern int TgtIndex[MAXDELIM];
+extern int nTgtIndex;
+
+#endif
+
+
+
+
+
+
+
+
+
---
+++ src/AsymMain.h
@@ -0,0 +1,115 @@
+#ifndef ASYM_MAIN_H
+#define ASYM_MAIN_H
+
+// Default Values for Run Condition
+datprocStruct dproc = { 
+    400,900,   // energy
+    -30,30,     // banana cut width (Constant mode only)
+    0, // FEEDBACKMODE
+    0, // CMODE
+    0, // DMODE
+    0, // TMODE
+    0, // AMODE
+    1, // BMODE
+    0, // ZMODE
+    0, // MESSAGE
+    2, // CBANANA
+    0, // UPDATE
+    1, // MMODE
+    0, // NTMODE
+    1, // RECONFMODE
+    0, // RAMPMODE
+    0, // STUDYMODE
+    3, // MassSigma banana curve cut within <MassSigma> away from the 12C mass
+    1.5e6,   // 1-sigma of 12C mass peak => 1.5e6 [keV] 
+    0,       // Time shift in [ns]
+    999.05,  // Wall Current Monitor process Fill range +/-5[%] 
+    8,       // Lower Mass limit for peak position adjustment fit default :8 GeV
+    100,     // <thinout> event rate to be feed into feedback routine
+};  // data process modes 
+
+StructExtInput extinput = {
+  0, // CONFIG
+  0, // MASSCUT
+  0. // TSHIFT
+} ;
+
+StructReadFlag ReadFlag = {
+  0, // RECBEGIN
+  0, // PCTARGET 
+  0, // WCMADO
+  0, // READADO
+  0  // RHICCONF
+};
+
+StructFlag Flag = {
+  0       // feedback mode
+};
+
+// phi angle of each strips for Run05 (l=18.5cm)
+float phiRun5[72]={
+0.72601,0.73679,0.74758,0.75838,0.76918,0.77999,0.79080,0.80161,0.81242,0.82322,0.83401,0.84479,
+1.51141,1.52219,1.53298,1.54378,1.55458,1.56539,1.57620,1.58701,1.59782,1.60862,1.61941,1.63019,
+2.29680,2.30758,2.31837,2.32917,2.33998,2.35079,2.36160,2.37241,2.38321,2.39401,2.40480,2.41558,
+3.86760,3.87838,3.88917,3.89997,3.91078,3.92159,3.93240,3.94321,3.95401,3.96481,3.97560,3.98638,
+4.65300,4.66378,4.67457,4.68537,4.69617,4.70698,4.71779,4.72860,4.73941,4.75021,4.76100,4.77178,
+5.43840,5.44918,5.45997,5.47077,5.48157,5.49238,5.50319,5.51400,5.52481,5.53561,5.54640,5.55718
+} ;
+
+// phi angle of each strips in trancated angles {45,90,135,225,270,315} deg.
+float phiRun5t[72]={
+0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,0.78540,
+1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,1.57080,
+2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,2.35619,
+3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,3.92699,
+4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,4.71239,
+5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779,5.49779
+} ;
+
+
+atdata_struct atdata;
+StructRunInfo runinfo;
+StructAverage average;
+StructFeedBack feedback;
+StructRunConst runconst;
+StructAnalysis analysis;
+
+int spinpat[120]; // spin pattern 120 bunches (ADO info)
+int fillpat[120]; // spin pattern 120 bunches (ADO info)
+int wcmfillpat[120]; // spin pattern 120 bunches (ADO info)
+float wcmdist[120];  // wall current monitor 120 bunches (ADO info)
+
+long int Ncounts[6][120]; // counts 6detectors 120 bunches
+long int NTcounts[6][120][NTBIN];  // counts 6detectors 120 bunches 6 tranges
+long int NRcounts[6][120][RAMPTIME]; // counts for 6det 120bunch RAMPTIME sec
+long int NDcounts[6][120][MAXDELIM]; // counts for 6 det 120 bunch per DELIMiter  
+long int NStrip[3][72]; // counts 72 strips 3 spin states
+
+char * confdir;
+char datafile[256];   // data file name 
+char reConfFile[256];    // overwrite configuration for T0 info
+char conf_file[256];  // overwrite configuration file 
+
+float ramptshift[500];  // ramp timing shift 
+
+long int Nevcut=0;       // number of events after 1st cut (whole data)
+long int Nevtot=0;       // number of total events (whole data) 
+long int Nread=0;        // real total events (completely everything)
+long int Nwcmtot=0;      // number of total wall current monitor data.
+int Nskip=1;             // number of events to be skipped in data process 
+long int Ngood[120];     // number of events after carbon cut (each bunch)
+long int Ntotal[120];    // number of events before carbon cut (each bunch)
+long int Nback[120];     // number of events below the curbon cut (each bunch)
+int NFilledBunch=0;      // number of Filled Bunch
+
+long int * pointer ;
+int ndelim ;
+long int VtgtLinear[MAXDELIM]; 
+long int VtgtRotary[MAXDELIM];
+long int HtgtLinear[MAXDELIM];
+long int HtgtRotary[MAXDELIM];
+int TgtIndex[MAXDELIM];
+int nTgtIndex = 0 ;
+
+
+#endif /* ASYM_MAIN_H */
