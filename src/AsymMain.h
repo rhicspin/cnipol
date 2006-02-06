486ddf4b5e4a37b66b600c0bcf1d682d26b7ce52
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-06 20:43:41 +0000

    New valiable CalibFile.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@13 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/AsymMain.h
+++ src/AsymMain.h
@@ -86,9 +86,11 @@ long int NDcounts[6][120][MAXDELIM]; // counts for 6 det 120 bunch per DELIMiter
 long int NStrip[3][72]; // counts 72 strips 3 spin states
 
 char * confdir;
+char * calibdir;
 char datafile[256];   // data file name 
 char reConfFile[256];    // overwrite configuration for T0 info
 char conf_file[256];  // overwrite configuration file 
+char CalibFile[256];  // energy calibration file
 
 float ramptshift[500];  // ramp timing shift 
 
