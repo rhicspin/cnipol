6b084d23aebadda8be3814de425637cb7e39db3f
e950 <e950>
2006-02-09 18:09:22 +0000

    Loads Macro directory from environmental variable MACRODIR.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@35 cd24ecce-3d09-47a0-95ec-c030155e877a

--- kumac/ExeKinFit.C
+++ kumac/ExeKinFit.C
@@ -2,24 +2,25 @@
 void ExeKinFit(Char_t *run, Float_t bene, Int_t RHICBeam)
 //void exe_kinfit()
 {
-    
-    gROOT->LoadMacro("KinFit.C");
 
-    // select root file 
-    Char_t filename[100];
-    sprintf (filename, "douts/%s.root",run);
-    TFile *f = TFile::Open(filename);
-    
-    cout << "RUN: "<<run<< " ENE: "<<bene<< " file: "<<filename<<endl; 
-    
-    KinFit *kfit = new KinFit(run, bene, RHICBeam);
-    kfit->Fit(0);    // arg = 0 ... new fit with two free paramters 
-    kfit->Fit(1);    // fix dlayer values with average
+  char MACRO[100];
+  sprintf(MACRO,"%s/KinFit.C",gSystem->Getenv("MACRODIR"));
+  gROOT->LoadMacro(MACRO);
 
-    kfit->PlotResult();
+  // select root file 
+  Char_t filename[100];
+  sprintf (filename, "douts/%s.root",run);
+  TFile *f = TFile::Open(filename);
+    
+  cout << "RUN: "<<run<< " ENE: "<<bene<< " file: "<<filename<<endl; 
+  KinFit *kfit = new KinFit(run, bene, RHICBeam);
+  kfit->Fit(0);    // arg = 0 ... new fit with two free paramters 
+  kfit->Fit(1);    // fix dlayer values with average
 
+  kfit->PlotResult();
 
     
-    delete kfit;
+  delete kfit;
+
 }
 
