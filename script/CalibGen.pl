3af17cb0860ec1dda0442ee00fc359b7e8862e4b
e950 <e950>
2006-02-09 23:43:52 +0000

    implemented $MACRODIR in the script.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@41 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/CalibGen.pl
+++ script/CalibGen.pl
@@ -1,22 +1,48 @@
 #! /usr/bin/perl
 
-$Runn = $ARGV[0];
-
-$basedir = $basedir = "/home/itaru/pol2004-offl";
-
-## need to be changed
-##----------------------------------
-##$datafile = "/data/2005/calibr/$Runn.data";
-#$datafile = "/data/2005/data/$Runn.data";
-$datafile = "/home/itaru/data/$Runn.data";
-##----------------------------------
-$logfile = "$basedir/Macro/Douts/$Runn.calib.log";
-$command = "Asym";
 
 
-if (-e $datafile) {
+#----------------------------------------------------------------------
+#               Command Line Options
+#----------------------------------------------------------------------
+use Getopt::Std;
+my %opt;
+getopts('f:h', \%opt);
+if ( $opt{h} ) {
+    help();
+}
+
+# Get Run ID
+my $Runn = $opt{f};
+if (length ($Runn) == 0){
+    print "Error: Specify calibration <filename>.\n";
+    help();
+}
+
+sub help(){
+    print "\n";
+    print " Usage:\n  $0 -h [ -f <runID>]\n\n"; 
+    print "    create calibration energy histograms for fit. Execute CalibCal.pl next.\n\n";
+    print "\t -f <filename> energy calibration filename.\n";
+    print "\t -h           Show this help\n";
+    print "\n\n";
+    print "    ex.) CalibCal.pl -f blue_calib_0504\n";
+    print "\n";
+    exit(0);
+}
+
+
+$DATADIR      = $ENV{"DATADIR"}; 
+$BASEDIR      = $ENV{"ASYMDIR"};
+
+$CHECKDATA= "$DATADIR/$Runn.data";
+$DATAFILE = "$Runn.data";
+$LOGFILE  = "$BASEDIR/douts/$Runn.calib.log";
+$COMMAND  = "Asym";
+
+if (-e $CHECKDATA) {
     
-    $datsize = (stat($datafile))[7];
+    $datsize = (stat($CHECKDATA))[7];
 
     if ($datsize < 5000){
 	# do not proceed just give warning
@@ -32,13 +58,13 @@ if (-e $datafile) {
 	$options = " -C ";     # TOF vs. EDEP without Tzero subtraction
 	
 	
-	printf "nice -19 $command $NEVOPT -f $datafile $options  -o $Runn.calib.hbook $tshiftopt| tee $logfile\n "; 
+	printf "nice -19 $COMMAND $NEVOPT -f $DATAFILE $options  -o $Runn.calib.hbook $tshiftopt| tee $LOGFILE\n "; 
 
 	##### START #####
-	system ("nice -19 $command $NEVOPT -f $datafile $options -o $Runn.calib.hbook $tshiftopt| tee $logfile "); 
+	system ("nice -19 $COMMAND $NEVOPT -f $DATAFILE $options -o $Runn.calib.hbook $tshiftopt| tee $LOGFILE "); 
 	
 	system ("h2root $Runn.calib.hbook ");
-	system ("mv $Runn.calib.root ./Douts");
+	system ("mv $Runn.calib.root ./douts");
 	system ("rm $Runn.calib.hbook");
 
     }
