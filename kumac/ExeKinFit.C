4ca1de4001fc96053585b47a0fa0e7ae334e8b32
e950 <e950>
2006-02-09 23:48:26 +0000

    additional argument HID.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@44 cd24ecce-3d09-47a0-95ec-c030155e877a

--- kumac/ExeKinFit.C
+++ kumac/ExeKinFit.C
@@ -1,19 +1,18 @@
 
-void ExeKinFit(Char_t *run, Float_t bene, Int_t RHICBeam)
-//void exe_kinfit()
+void ExeKinFit(Char_t *run, Float_t bene, Int_t RHICBeam, Int_t HID)
 {
 
   char MACRO[100];
   sprintf(MACRO,"%s/KinFit.C",gSystem->Getenv("MACRODIR"));
   gROOT->LoadMacro(MACRO);
 
-  // select root file 
+  // open root file 
   Char_t filename[100];
   sprintf (filename, "douts/%s.root",run);
   TFile *f = TFile::Open(filename);
     
   cout << "RUN: "<<run<< " ENE: "<<bene<< " file: "<<filename<<endl; 
-  KinFit *kfit = new KinFit(run, bene, RHICBeam);
+  KinFit *kfit = new KinFit(run, bene, RHICBeam, HID);
   kfit->Fit(0);    // arg = 0 ... new fit with two free paramters 
   kfit->Fit(1);    // fix dlayer values with average
 
