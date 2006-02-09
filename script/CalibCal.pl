4977d8d5ed51df8087226eb2ce2688a1708bce5d
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-09 23:43:52 +0000

    implemented $MACRODIR in the script.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@41 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/CalibCal.pl
+++ script/CalibCal.pl
@@ -1,21 +1,54 @@
 #! /usr/bin/perl
+$MACRODIR     = $ENV{"MACRODIR"};
 
-$Runn = $ARGV[0];
+
+#----------------------------------------------------------------------
+#               Command Line Options
+#----------------------------------------------------------------------
+use Getopt::Std;
+my %opt;
+getopts('f:h', \%opt);
+
+if ( $opt{h} ) {
+    help();
+}
+
+# Get Run ID
+my $Runn = $opt{f};
+if (length ($Runn) == 0){
+    print "Error: Specify calibration <filename>.\n";
+    help();
+}
+
+sub help(){
+    print "\n";
+    print " Usage:\n  $0 -h [ -f <runID>]\n\n"; 
+    print "    Execute fit on calibration histograms. \n";
+    print "    Run CalibGen.pl to create calibration histogram data file.\n\n";
+    print "\t -f <filename> calibration file name w/o .data \n";
+    print "\t -h         Show this help";
+    print "\n\n";
+    print "    ex.) CalibCal.pl -f bluc_calib_0504\n";
+    print "\n";
+    exit(0);
+}
+
+
+
+#########################################################################
+######                     Main Routine                           #######
+#########################################################################
 
 printf("CALIB DATA : $Runn \n");
-#printf("Are these correct? (if yes type Y/y, if no CTRL-C )\n");
-
-#$ANSWER = <STDIN>; chop($ANSWER);
-
-#if ($ANSWER eq 'Y' || $ANSWER eq 'y') {
+system("echo '.x $MACRODIR/ExeCalib.C(\"$Runn\")' > input.C");
+system("root -b < input.C | tee calib/$Runn.fit.log");
     
-    system("echo '.x exe_calib.C(\"$Runn\")' > input.C");
-    system("root -b < input.C | tee Calib/$Runn.fit.log");
-    
-    system("mv testfit.dat Calib/$Runn.temp.dat");
-    system("mv testfit.ps  Calib/$Runn.fittemp.ps");
-    system("mv testsummary.ps Calib/$Runn.summarytemp.ps");
-    system("rm input.C");
-#}
+system("mv testfit.dat calib/$Runn.temp.dat");
+system("mv testfit.ps  calib/$Runn.fittemp.ps");
+system("mv testsummary.ps calib/$Runn.summarytemp.ps");
+system("rm input.C");
 
-#printf ("$ANSWER\n");
+
+
+
+
