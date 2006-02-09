e952241b2f546ec5445f50dec17edbd9f27357ad
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-09 14:56:20 +0000

    installation script.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@28 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -d $INSTALLDIR ] ; then
+	echo "install executables to $INSTALLDIR"
+else
+	echo "Create $INSTALLDIR";
+	mkdir $INSTALLDIR
+fi
+
+make -C src 
+make -C src install
+
+cd script 
+install *.pl *.sh -C -v  --mode 777 $INSTALLDIR 
+cd ..
+
+
+# install paw macros:
+if [ -d $MACRODIR ] ; then
+	echo "install paw macros to $MACRODIR";
+else
+	echo "Create $MACRODIR";
+	mkdir $MACRODIR
+fi
+
+cd macro
+install *.pl *.sh -C -v  --mode 777 $MACRODIR
+install *.kumac *.C -C -v  --mode 664 $MACRODIR
+cd ..
+
+echo "Installation Completed"
+
