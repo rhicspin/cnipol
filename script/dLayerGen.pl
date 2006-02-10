fcc25a830d791ac618c16d071fb2b9e43e692a89
e950 <e950>
2006-02-10 00:14:29 +0000

    omitted commandline input of config file. config file should be loaded through run.db.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@47 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/dLayerGen.pl
+++ script/dLayerGen.pl
@@ -1,8 +1,5 @@
 #! /usr/bin/perl
 
-$Runn = $ARGV[0];
-$ConfigFileName = $ARGV[1];
-
 #----------------------------------------------------------------------
 #               Command Line Options
 #----------------------------------------------------------------------
@@ -33,13 +30,6 @@ sub help(){
     exit(0);
 }
 
-
-
-if (length ($ConfigFileName) != 0) {
-    $ConfigFile = "$ConfigFileName.dat";
-    $OPT_CONFIG = " -F $ConfigFile ";
-}
-
 $DATADIR      = $ENV{"DATADIR"}; 
 $BASEDIR      = $ENV{"ASYMDIR"};
 
@@ -52,13 +42,12 @@ if (-e $CHECKDATA) {
     
 	# Number of events to be skipped 1: all the events
 	$NEVOPT = " -n 1 ";    
-	$opt_bwidth="3";     # banana mass cut <sigma> 
 	$options = "-D -A";     # TOF vs. EDEP without Tzero subtraction
 	
-	printf "nice -19 $COMMAND $NEVOPT -f $DATAFILE -m $opt_bwidth $options  -o $Runn.hbook $tshiftopt $OPT_CONFIG | tee $LOGFILE\n "; 
+	printf "nice -19 $COMMAND $NEVOPT -f $DATAFILE $options  -o $Runn.hbook $tshiftopt $OPT_CONFIG | tee $LOGFILE\n "; 
 	
 	##### START #####
-	system ("nice -19 $COMMAND $NEVOPT -f $DATAFILE -m $opt_bwidth $options  -o $Runn.hbook $tshiftopt $OPT_CONFIG | tee $LOGFILE\n ");
+	system ("nice -19 $COMMAND $NEVOPT -f $DATAFILE  $options  -o $Runn.hbook $tshiftopt $OPT_CONFIG | tee $LOGFILE\n ");
 	system ("h2root $Runn.hbook ");
 	system ("mv $Runn.root ./douts");
 	system ("rm $Runn.hbook");
