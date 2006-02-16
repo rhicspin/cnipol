ce2102ad790eb01fa9b8eda738c76bc6316784e9
e950 <e950>
2006-02-16 18:29:41 +0000

    Now fit energy range and kinematic constant E2T will be feed through dLayerCal.pl to ExeKinFit.C. New option -E <Emax:Emin> has been implemented. This option is only valid from dLayerCal.pl and dLayer.pl, but not from mkConfig.pl, since energy dependence study usually done within deadlayer evaluation.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@82 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/dLayerCal.pl
+++ script/dLayerCal.pl
@@ -1,13 +1,17 @@
 #! /usr/bin/perl
-# defualts
-$HID  = 15000;
+# defualts 
+# HID:15000 (const.t) HID:15100: (banana)
+$HID  = 15000; 
+# Fit Energy Rnage Default
+$EMIN=400;
+$EMAX=900;
 
 #----------------------------------------------------------------------
 #               Command Line Options
 #----------------------------------------------------------------------
 use Getopt::Std;
 my %opt;
-getopts('f:bh', \%opt);
+getopts('E:f:bh', \%opt);
 
 if ( $opt{h} ) {
     help();
@@ -24,16 +28,26 @@ if (length ($Runn) == 0){
     help();
 }
 
+
+# Get Fit Energy Range
+if ( $opt{E} ){
+    @field = split(/:/,$opt{E});
+    if (length($field[0]) != 0){ $EMIN=$field[0];}
+    if (length($field[1]) != 0){ $EMAX=$field[1];}
+}
+
+
 sub help(){
     print "\n";
-    print " Usage:\n  $0 -hb [ -f <runID>]\n\n"; 
+    print " Usage:\n  $0 -hb [ -f <runID>] [-E <Emin:Emax>]\n\n"; 
     print "    fit banana histograms and get deadlayer and t0.\n";
     print "    Execute dLayerGen.pl for creation.\n\n";
-    print "\t -f <runID> runID\n";
-    print "\t -b         fit on banana cut events. (def:const.t cut)\n";
-    print "\t -h         Show this help \n";
+    print "\t -f <runID>     runID\n";
+    print "\t -b             Fit on banana cut events. (def:const.t cut)\n";
+    print "\t -E <Emin:Emax> Fit Energy Range in [keV] (def <$EMIN:$EMAX>) \n";
+    print "\t -h             Show this help \n";
     print "\n";
-    print "    ex.) dLayerCal.pl -f 7279.005 -b \n";
+    print "    ex.) dLayerCal.pl -E 350:950 -f 7279.005 -b \n";
     print "\n";
     exit(0);
 }
@@ -68,6 +82,9 @@ while (<LOGFILE>) {
     } elsif (/RHIC Beam/) {
 	($F1,$F2,$F3,$F4) = split;
 	$RHICBeam=$F4;
+    } elsif (/Kinematic Const. E2T/) {
+	($F1,$F2,$F3,$F4,$F5) = split;
+	$E2T=$F5;
     };
 };
 
@@ -76,13 +93,16 @@ printf("RUN NUMBER  : $Runn \n");
 printf("Mass Cut    : $MASSCUT \n");
 printf("Beam Energy : $Bene \n");
 printf("RHICBeam    : $RHICBeam \n");
+printf("E2T         : $E2T \n");
+printf("Emin - Emax : $EMIN - $EMAX \n");
 printf("HID         : $HID \n");
+
 
 #----------------------------------------------------------------------
 #               Main Routine
 #----------------------------------------------------------------------
 
-system("echo '.x $MACRODIR/ExeKinFit.C(\"$Runn\", $Bene, $RHICBeam, $HID)' > input.C");
+system("echo '.x $MACRODIR/ExeKinFit.C(\"$Runn\", $Bene, $RHICBeam, $E2T, $EMIN, $EMAX, $HID)' > input.C");
 system("root -b < input.C | tee $DLAYERDIR/$Runn.fit.log");
     
 system("mv testfit.dat $DLAYERDIR/$Runn.temp.dat");
