0963f47a5c889e313c7c1f197ccfd79ab34296f4
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-16 18:29:41 +0000

    Now fit energy range and kinematic constant E2T will be feed through dLayerCal.pl to ExeKinFit.C. New option -E <Emax:Emin> has been implemented. This option is only valid from dLayerCal.pl and dLayer.pl, but not from mkConfig.pl, since energy dependence study usually done within deadlayer evaluation.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@82 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/dLayer.pl
+++ script/dLayer.pl
@@ -1,6 +1,11 @@
 #! /usr/bin/perl
 # defualts
-$HID  = 15000;
+# defualts 
+# HID:15000 (const.t) HID:15100: (banana)
+$HID  = 15000; 
+# Fit Energy Rnage Default
+$EMIN=400;
+$EMAX=900;
 $OPT  = " ";
 
 #----------------------------------------------------------------------
@@ -8,14 +13,14 @@ $OPT  = " ";
 #----------------------------------------------------------------------
 use Getopt::Std;
 my %opt;
-getopts('f:bh', \%opt);
+getopts('E:f:bh', \%opt);
 
 if ( $opt{h} ) {
     help();
 }
 
 if ( $opt{b} ){
-    $OPT="-b";
+    $OPT="-b $OPT";
 }
 
 # Get Run ID
@@ -25,16 +30,23 @@ if (length ($Runn) == 0){
     help();
 }
 
+# Get Fit Energy Range
+if ( $opt{E} ){
+    $OPT="-E $opt{E} $OPT";
+}
+
+
 sub help(){
     print "\n";
-    print " Usage:\n  $0 -hb [ -f <runID>]\n\n"; 
+    print " Usage:\n  $0 -hb [-f <runID>] [-E <Emin:Emax>]\n\n"; 
     print "    Generate histograms for deadlayer fit and execute fit.\n";
     print "    Executs dLayerGen.pl and dLayerCal.pl \n\n";
     print "\t -f <runID> runID\n";
     print "\t -b         fit on banana cut events. (def:const.t cut)\n";
+    print "\t -E <Emin:Emax> Fit Energy Range in [keV] (def <$EMIN:$EMAX>) \n";
     print "\t -h         Show this help \n";
     print "\n";
-    print "    ex.) dLayer.pl -f 7279.005 -b \n";
+    print "    ex.) dLayer.pl -f 7279.005 -b -E 350:950 \n";
     print "\n";
     exit(0);
 }
@@ -49,4 +61,3 @@ system("echo 'Generating histograms...\n'");
 system("dLayerGen.pl -f $Runn \n");
 system("echo 'Executing Fitting...\n'");
 system("dLayerCal.pl $OPT -f $Runn \n");
-
