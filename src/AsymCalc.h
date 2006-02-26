a6f7f2e3a2c68f4e7b44c8f5372c704904ab8362
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-02-26 01:03:46 +0000

    new header file for AsymCalc.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@151 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ src/AsymCalc.h
@@ -0,0 +1,41 @@
+// Header file for AsymCalc
+// Author   : Itaru Nakagawa
+// Creation : 02/25/2006         
+
+#ifndef ASYM_CALC_H
+#define ASYM_CALC_H
+
+extern void HHBOOK1(int hid, char* hname, int xnbin, float xmin, float xmax) ;
+extern void HHPAK(int, float*);
+extern void HHPAKE(int, float*);
+extern void HHF1(int, float, float);
+//extern void HHKIND(int, int*, char*);
+extern float HHMAX(int);
+extern float HHSTATI(int hid, int icase, char * choice, int num);
+extern void HHFITHN(int hid, char*chfun, char*chopt, int np, float*par, 
+	float*step, float*pmin, float*pmax, float*sigpar, float&chi2);
+extern void HHFITH(int hid, char*fun, char*chopt, int np, float*par, 
+	float*step, float*pmin, float*pmax, float*sigpar, float&chi2);
+
+
+float RawP[72], dRawP[72]; // Raw Polarization (Not corrected for phi)
+float FitChi2;
+void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag);
+
+
+
+class AsymFit
+{
+
+ private:
+
+ public:
+  void SinPhiFit(Float_t p0, Float_t *par, Float_t *phi, Float_t &chi2);
+  Float_t sinx(Float_t, Double_t *);
+
+};
+
+
+
+
+#endif /* ASYM_CALC_H */
