b49466703a38fdd5016d16eea82a3f5e55a3f019
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-03-12 16:39:09 +0000

    To avoid H/V confusion, the target position outputs are x-position and y-position from Asym. Forget about Horizontal or Vertical target. Just worry about H/V position.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@210 cd24ecce-3d09-47a0-95ec-c030155e877a

--- macro/filter.sh
+++ macro/filter.sh
@@ -4,7 +4,7 @@ ifile=$1
 ofile=tgt.dat
 
 cat $ifile | grep @ | gawk '{printf("%6d%10d%8d%8d\n", $2, $3, $4, $5)}' > x.dat
-echo "*Index  delimiter  x-rot    y-rot" > $ofile
+echo "*Index  delimiter  x-pos    y-pos" > $ofile
 $INSTALLDIR/interval >> $ofile
 rm -f x.dat
 
