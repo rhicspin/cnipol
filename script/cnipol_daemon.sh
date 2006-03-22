12ed13c3db8c81073a96ecf2d007a997503d1c78
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-03-22 00:34:06 +0000

    iteration is set to 3 now. Also it writes out converging results every iteration to .cnipol_daemon_dlayer.dat.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@247 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/cnipol_daemon.sh
+++ script/cnipol_daemon.sh
@@ -5,6 +5,8 @@
 
 
 CNI_DAEMON_RUNLIST=$ASYMDIR/.cnipol_daemon_run.list;
+CNI_DAEMON_DLAYER_STUDY=$ASYMDIR/.cnipol_daemon_dlayer.dat;
+DLAYERDIR=$ASYMDIR/dlayer
 HBOOKDIR=$ASYMDIR/hbook
 LOGDIR=$ASYMDIR/log
 FROM_FILL=7605;
@@ -26,13 +28,15 @@ fi
 #############################################################################
 help(){
     echo    " "
-    echo    " cnipol_daemon.sh [-xh][--dlayer-fit][-F <Fill#>][--fill-from <Fill#>][--fill-till <Fill#>]"
+    echo    " cnipol_daemon.sh [-xh][-F <Fill#>][--fill-from <Fill#>][--fill-till <Fill#>]"
+    echo    "                  [--dlayer-fit][-s --sleep <time>]"; 
     echo    "    : search for new run which has not been analyized and then run analysis program "
     echo    " "
     echo -e "   -F <Fill#>                Show list <Fill#>"
     echo -e "   --delayer-fit             run deadlayer fit"
     echo -e "   --fill-from <Fill#>       Make list from <Fill#> [def]:$FROM_FILL";
     echo -e "   --fill-till <Fill#>       Make list till <Fill#> [def]:$TILL_FILL";
+    echo -e "   -s --sleep <time>         Sleep <time> in sec [def]:$SLEEP_TIME";
     echo -e "   -h | --help               Show this help"
     echo -e "   -x                        Show example"
     echo    " "
@@ -58,11 +62,28 @@ ShowExample(){
 #############################################################################
 RunDlayer(){
 
-    echo $RunID ;
+    FITLOGFILE=$DLAYERDIR/$RunID.fit.log;
+
+    echo $RunID  ;
+    echo -e -n "$RunID " >> $CNI_DAEMON_DLAYER_STUDY;
     dLayer.pl -f $RunID 
     mkConfig.pl -f $RunID
+    AVE_Dl_1=`grep "dlave =" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+    AVE_T0_1=`grep " t0 average=" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
     dLayer.pl -f $RunID -b -F ./config/$RunID.config.dat
-    mkConfig.pl -f $RunID
+    mkConfig.pl -f $RunID ;
+    AVE_Dl_2=`grep "dlave =" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+    AVE_T0_2=`grep " t0 average=" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+    Dl_ratio=`echo -e -n "$AVE_Dl_1 $AVE_Dl_2" | gawk '{printf("%7.4f",$2/$1)}'`
+    T0_ratio=`echo -e -n "$AVE_T0_1 $AVE_T0_2" | gawk '{printf("%7.4f",$2/$1)}'`
+    echo -e -n " $Dl_ratio  $T0_ratio " >> $CNI_DAEMON_DLAYER_STUDY;
+    dLayer.pl -f $RunID -b -F ./config/$RunID.config.dat
+    mkConfig.pl -f $RunID 
+    AVE_Dl_3=`grep "dlave =" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+    AVE_T0_3=`grep " t0 average=" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+    Dl_ratio=`echo -e -n "$AVE_Dl_2 $AVE_Dl_3" | gawk '{printf("%7.4f",$2/$1)}'`
+    T0_ratio=`echo -e -n "$AVE_T0_2 $AVE_T0_3" | gawk '{printf("%7.4f",$2/$1)}'`
+    echo -e -n " $Dl_ratio  $T0_ratio \n" >> $CNI_DAEMON_DLAYER_STUDY; 
 
 }
 
@@ -128,6 +149,7 @@ while test $# -ne 0; do
   --fill-from) shift ; FROM_FILL=$1;;
   --fill-till) shift ; TILL_FILL=$1;;
   --dlayer-fit) ExeDlayerFit=1;;
+  -s | --sleep) shift ; SLEEP_TIME=$1 ;;
   -x) shift ; ShowExample ;;
   -h | --help) help ;;
   *)  echo "Error: Invarid Option $1"
