ba315bb06377602cf2ffd12e46f875bbe9e1ad2b
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-03-22 00:32:09 +0000

    added t0 and Delta_t0 in outputs.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@246 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/dLayerAve.sh
+++ script/dLayerAve.sh
@@ -51,6 +51,8 @@ InitVariables(){
     FILL_BUNCH=0;
     SPECIFIC_LUMI=0;
     TGT_POS=0;
+    AVE_T0=0;
+    DELTA_T0=0;
 
 }
 
@@ -60,18 +62,30 @@ InitVariables(){
 #############################################################################
 ShowIndex(){
 
-    printf "=============================================================\n";
+    printf "=============================================================";
+    printf "===============\n"
     printf " RunID    ";
     printf " Dl_ave";
     printf " err";
-    printf " Rate ";
+    printf " Rate   ";
+    printf " WCM " ;
+    printf " SpeLumi ";
+    printf " #of "
+    printf " T0_ave";
+    printf " DeltaT0";
     printf "\n";
     printf "            ";
     printf "[ug/cm^2]";
     printf "";
-    printf " [Hz]";
+    printf " [Hz]   ";
+    printf " ave ";
+    printf "        ";
+    printf " Fill  ";
+    printf " [ns]   ";
+    printf " [ns]";
     printf "\n";
-    printf "=============================================================\n";
+    printf "=============================================================";
+    printf "===============\n"
 
 }
 
@@ -103,14 +117,16 @@ for (( i=1; i<=$NLINE ; i++ )) ;
 	    READ_RATES=`grep 'Read Rate' $LOGFILE | gawk '{printf("%4.2f", $5*1e-6)}'`
 	    AVE_Dl=`grep "dlave =" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`
 	    AVE_Dl_ERROR=`grep "Deviation/strip=" $FITLOGFILE | gawk '{printf("%5.2f", $2)}'`
-	    AVE_WCM=`grep 'WCM Average  ' $LOGFILE | gawk '{printf("%7.2f", $4)}'`
+	    AVE_WCM=`grep 'WCM Average  ' $LOGFILE | gawk '{printf("%7.1f", $4)}'`
 	    FILL_BUNCH=`grep '# of Filled Bunch' $LOGFILE | gawk '{printf(" %3d", $6)}'`
 	    SPECIFIC_LUMI=`grep 'Specific Luminosity' $LOGFILE | gawk '{printf("%6.3f",$6)}'`
 	    TGT_POS=`grep @ $LOGFILE | gawk '{print $4}'`
+	    AVE_T0=`grep " t0 average=" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
+	    DELTA_T0=`grep " Delta_t0 average=" $FITLOGFILE | gawk '{printf("%6.2f",$3)}'`;
 
 	    echo -e -n "$RUNID $AVE_Dl $AVE_Dl_ERROR $READ_RATES " 
-	    echo -e -n "$AVE_WCM $SPECIFIC_LUMI $FILL_BUNCH "
-	    echo -e -n "$DUMMY $DUMMY $DUMMY $DUMMY $DUMMY "
+	    echo -e -n "$AVE_WCM $SPECIFIC_LUMI $FILL_BUNCH   "
+	    echo -e -n "$AVE_T0 $DELTA_T0 $DUMMY $DUMMY $DUMMY "
 	    echo -e -n "$DUMMY $DUMMY $DUMMY $DUMMY $DUMMY "
 	    echo -e -n "\n";
 
