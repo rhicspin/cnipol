77d26bf157f436b4c2d7e42e70fa134ac900542a
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-03-29 16:59:50 +0000

    on it's way to speed up.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@270 cd24ecce-3d09-47a0-95ec-c030155e877a

--- macro/KinFit.C
+++ macro/KinFit.C
@@ -36,6 +36,54 @@ int DisableStr[NDisableList][NDisableStrip] =
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //   FUNCTION to convert from (Edep, Dwidth) to Ekin
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+const Double_t cp0[4] = {-0.5174,0.4172,0.3610E-02,-0.1286E-05}; 
+const Double_t cp1[4] = {1.0000,0.8703E-02,0.1252E-04,0.6948E-07};
+const Double_t cp2[4] = {0.2990E-05,-0.7937E-05,-0.2219E-07,-0.2877E-09};
+const Double_t cp3[4] = {-0.8258E-08,0.4031E-08,0.9673E-12,0.3661E-12};
+const Double_t cp4[4] = {0.3652E-11,-0.8652E-12,0.4059E-14,-0.1294E-15};
+
+Double_t 
+KinFunc(Double_t *x, Double_t *par){
+    
+    Double_t pp[5];
+    
+    Double_t par0;
+    Double_t x0 = x[0];
+    if (par[0]>=0.) {
+        par0 = TMath::Abs(par[0]);
+        //par0 = fabs(par[0]);
+    } else {
+        par0 = 0.;
+    }
+
+    Double_t p2 = par0*par0;
+    Double_t p3 = par0*p2;
+    pp[0] = cp0[0] + cp0[1]*par0 + cp0[2]*p2 + cp0[3]*p3;
+    pp[1] = cp1[0] + cp1[1]*par0 + cp1[2]*p2 + cp1[3]*p3;
+    pp[2] = cp2[0] + cp2[1]*par0 + cp2[2]*p2 + cp2[3]*p3;
+    pp[3] = cp3[0] + cp3[1]*par0 + cp3[2]*p2 + cp3[3]*p3;
+    pp[4] = cp4[0] + cp4[1]*par0 + cp4[2]*p2 + cp4[3]*p3;
+    
+    Double_t x2 = x0*x0;
+    Double_t Ekin = pp[0] + pp[1]*x0 + pp[2]*x2 
+        + pp[3]*x2*x0 + pp[4]*x2*x2;
+    Double_t tof;
+    if (Ekin != 0.0) {
+        tof = KinConst_E2T/sqrt(Ekin) + par[1];
+    } else {
+        tof =0.0;
+    }
+
+    return tof;
+
+};
+*/
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//   FUNCTION to convert from (Edep, Dwidth) to Ekin
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Double_t 
 KinFunc(Double_t *x, Double_t *par){
     
@@ -72,6 +120,7 @@ KinFunc(Double_t *x, Double_t *par){
     return tof;
 
 };
+
 
 
 // Extracting the Coefficients
@@ -413,7 +462,7 @@ void KinFit::FitOne(Int_t St, Int_t mode)
     Char_t hName[100];
     sprintf(hName,"h%d",HID+St+1);  
     
-    TH2D* h2d = (TH2D*) gDirectory->Get(hName);
+    TH2D* h2d = (TH2D*) gDirectory->Get("h15101");
     h2d -> SetStats(0);
     sprintf(hName,"%s ST-%d",runid, St+1);
     h2d -> SetTitle(hName);
