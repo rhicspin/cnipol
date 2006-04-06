3a55ba09082936e758eb3308b41e6bcc02bc3bfd
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-04-06 17:57:05 +0000

    first draft
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@292 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ script/anomfinder.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#find anomalous runs
+#A. Hoffman
+#April 6, 2006
+# For now, this program only accepts specific minimum values under 10%
+# All -n entries greater 10% will revert to 10% 
+
+INF="$ASYMDIR/mkDB.txt";
+B=5;
+DUMP=0;
+h=0;
+m=0;
+
+#############################################################################
+#                                     Help                                  #
+#############################################################################
+
+helpmenu() {
+
+    echo    " "
+    echo    " anomfinder.sh [-dhm][-n <#>]";
+    echo    "    : list runs with large difference in onlineP - offlineP ";
+    echo    " "
+    echo -e "   -h                        Show this help file";
+    echo -e "   -d                        dump list into anom.list"
+    echo -e "   -n <#>                    finds all lists with a difference greater than <#>";
+    echo -e "   -m                        updates the mkDB.txt database first (this will take some time)";
+    echo    " ";
+    echo -e "   default minimum difference is 5%";
+    echo    " ";
+    exit;
+}
+
+
+#############################################################################
+#                       GrepAnomRuns()                                      #
+#############################################################################
+
+GrepAnomRuns() {
+
+    if test $DUMP -eq 1
+    then
+	grep "[$B-9]\.[0-9][0-9]$\|1[0-9]\.[0-9][0-9]$" $INF > anom.list;
+	echo "found all runs with a difference of $B % or greater and dumped them into anom.list";
+	
+    else
+	grep "[$B-9]\.[0-9][0-9]$\|1[0-9]\.[0-9][0-9]$" $INF;
+	echo "found all runs with a difference of $B % or greater";
+    fi
+}
+
+GrepBigAnomRuns() {
+
+    if test $DUMP -eq 1
+    then
+	grep "[1-9][0-9]\.[0-9][0-9]$" $INF > anom.list;
+	echo "found all runs with difference of 10 % or greater and dumped them into anom.list";
+	
+    else
+	grep "[1-9][0-9]\.[0-9][0-9]$" $INF;
+	echo "found all runs with a difference of 10 % or greater";
+    fi
+}
+
+#############################################################################
+#                                    Main                                   #
+#############################################################################
+
+
+while getopts n:dhm opt
+do
+  case "$opt" in
+      n) B="$OPTARG" ;;
+      d) DUMP=1 ;;
+      h) helpmenu ;;
+      m) m=1;;
+  esac
+done
+
+
+if test $m -eq 1
+then
+    echo "updating mkDB.txt...";
+    mkDB.sh --exclusive > $INF
+fi
+
+if test $B -gt 9
+then
+    GrepBigAnomRuns;
+
+else
+    GrepAnomRuns;
+fi
