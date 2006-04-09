5a9a7beab090faaafb5c5c1bb12f41df775e5d77
e950 <e950>
2006-04-09 03:51:11 +0000

    Fixed 1 line shifted output. The last delimiter should be 1 sec.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@309 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/util/interval.cc
+++ src/util/interval.cc
@@ -4,32 +4,41 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int indexx,delim,xrot,yrot;
+struct StructData{
+  int index;
+  int delim;
+  int xrot;
+  int yrot;
+} pres, prev;
+
 
 int main(){
   
   ifstream in_file;
   int ch=0;
   int i=0;
-  int t=0;
-  int time=0;
-  int indexxx=0;
+  int interval=0;
   in_file.open("x.dat");
 
   while (( ch=in_file.peek()) != EOF )
     {
-      in_file >> indexx >> delim >> xrot >> yrot;
 
-      if (i) {
-	if (indexxx != indexx) cout << indexx << " " << delim-t <<" " <<  xrot << " " << yrot << endl;;
-      } else {
-	cout << indexx << " " << delim <<" " <<  xrot << " " << yrot << endl;;
-      }
-      t=delim;
-      indexxx=indexx;
+      in_file >> pres.index >> pres.delim >> pres.xrot >> pres.yrot;
+
+      interval = prev.delim != pres.delim ? pres.delim - prev.delim : 1;
+      if (i>0) 
+	cout << prev.index << " " << interval << " " << prev.xrot << " " << prev.yrot << endl;
+
+      prev.index = pres.index;
+      prev.delim = pres.delim;
+      prev.xrot  = pres.xrot;
+      prev.yrot  = pres.yrot;
 
       ++i;
+
     }
+
+  
 
 
  return 0;
