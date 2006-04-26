2043707ad26d542ae4d3faa739b54e9727212842
e950 <e950>
2006-04-26 14:33:58 +0000

    second draft with insert and append routines
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@383 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/rundb_inserter.sh
+++ script/rundb_inserter.sh
@@ -3,23 +3,27 @@
 #April 20, 2006, A. Hoffman
 
 INF=out.dat;
-DB=test.dat;
+DB=testdb.txt;
 LIST=runlist.txt;
 
+#Find the run number, the next run number, and the reference line number
+
 NLINE=`wc $DB | gawk '{print $1}'`
+grep -n '^.' $LIST > tmplist.txt
+
+RunN=`grep "^\[" $INF | sed -e "s/\[//" | sed "s/\]//" | sed -e "s/@//"`
+echo -e "Run number is $RunN"
+
+RefN=`grep -n "$RunN" $LIST | cut -f1 -d: | gawk '{print $1 +1}'`
+echo -e "Reference number is $RefN"
+
+NextN=`grep "^$RefN" tmplist.txt | sed "s/:/ /" | gawk '{print $2}'`
+echo -e "Next number is $NextN"
+
+tester=`grep "\[$NextN" $DB`
+echo -e "$tester"
 
 InsertTag()  {
-
-    grep -n '^.' $LIST > tmplist.txt
-
-    RunN=`grep "^\[" $INF | sed -e "s/\[//" | sed "s/\]//" | sed -e "s/@//"`
-    echo -e "$RunN"
-
-    RefN=`grep -n "$RunN" $LIST | cut -f1 -d: | gawk '{print $1 +1}'`
-    echo -e "$RefN"
-
-    NextN=`grep "^$RefN" tmplist.txt | sed "s/:/ /" | gawk '{print $2}'`
-    echo -e "$NextN"
 
     header=`grep -n "\[$NextN" $DB | sed -e "s/:/ /" | gawk '{print $1-1}'`
     echo "$header"
@@ -34,4 +38,21 @@ InsertTag()  {
     echo "insertion complete"
 }
 
-InsertTag
+AppendTag() {
+
+    head -n $NLINE $DB > tmp1.txt
+    echo "" >> tmp1.txt
+    grep '^.' $INF >> tmp1.txt
+    echo "appending complete"
+
+}
+
+if [ $tester ]; then
+    echo "insterting";
+    InsertTag;
+
+else
+    echo "appending";
+    AppendTag;
+
+fi
