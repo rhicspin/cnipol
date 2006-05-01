a302a6545b3c217f8577e407f8652ec075b5b6e4
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-05-01 20:08:51 +0000

    fixed frame issues.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@414 cd24ecce-3d09-47a0-95ec-c030155e877a

--- macro/DlayerMonitor.C
+++ macro/DlayerMonitor.C
@@ -15,7 +15,6 @@
 const Int_t N=1000;
 void GetScale(Float_t *x, Int_t N, Float_t margin, Float_t & min, Float_t & max);
 
-
 class DlayerMonitor
 {
 
@@ -23,11 +22,12 @@ private:
   Float_t RunID[N],Dl[N],DlE[N],ReadRate[N],WCM[N],SpeLumi[N],NBunch[N]; 
   Float_t AveT0[N],DeltaT0[N];
   Float_t dx[N],dy[N];
+  TH2D* frame ;
 
 public:
   Int_t Plot(Int_t, Int_t, Int_t, Char_t *, Int_t, TLegend *aLegend);
   Int_t DlayerPlot(Char_t*, Int_t);
-  Int_t RunBothBeam(Int_t Mode,  TPostScript ps);
+  Int_t BlueAndYellowBeams(Int_t Mode, TCanvas *CurC, TPostScript *ps);
   Int_t DrawFrame(Int_t Mode,Int_t ndata, Char_t*);
   Int_t DlayerMonitor();
   Int_t GetData(Char_t * DATAFILE);
@@ -165,36 +165,43 @@ DlayerMonitor::DrawFrame(Int_t Mode, Int_t ndata, Char_t *Beam){
 
   Float_t xmin, xmax, ymin, ymax;
   Float_t margin=0.05;
+  Char_t xtitle[100],ytitle[100];
+  Char_t title[100],htitle[100];
+  sprintf(title,"DeadLayer History (%s)",Beam);
 
   switch (Mode) {
   case 10:
     GetScale(RunID, ndata, margin, xmin, xmax);
     ymin=20; ymax=80;
-    Char_t xtitle[100]="Fill Number";
-    Char_t ytitle[100]="DeadLayer Thickness [ug/cm^2]";
+    sprintf(xtitle,"Fill Number");
+    sprintf(ytitle,"DeadLayer Thickness [ug/cm^2]");
+    sprintf(title,"DeadLayer History (%s)",Beam);
     break;
   case 20:
     xmin=0.0 ; xmax=1.5;
     ymin=60  ; ymax=75;
-    Char_t xtitle[100]="Event Rate [MHz]";
-    Char_t ytitle[100]="DeadLayer Thickness [ug/cm^2]";
+    sprintf(xtitle,"Event Rate [MHz]");
+    sprintf(ytitle,"DeadLayer Thickness [ug/cm^2]");
+    sprintf(title," DeadLayer Rate Dependence (%s)",Beam);
+    break;
   case 30:
     GetScale(RunID, ndata, margin, xmin, xmax);
-    ymin=-16  ; ymax=0;
-    Char_t xtitle[100]="Fill Number";
-    Char_t ytitle[100]="t0 Average [ns]";
+    ymin=-20  ; ymax=0;
+    sprintf(xtitle,"Fill Number");
+    sprintf(ytitle,"t0 Average [ns]");
+    sprintf(title," t0 Average History (%s)",Beam);
     break;
   case 40:
     GetScale(RunID, ndata, margin, xmin, xmax);
     GetScale(DeltaT0, ndata, margin, ymin, ymax);
-    Char_t xtitle[100]="Fill Number";
-    Char_t ytitle[100]="Delta_t0 [ns]";
+    sprintf(xtitle,"Fill Number");
+    sprintf(ytitle,"Delta_t0 [ns]");
+    sprintf(title," Delta_t0 History (%s)",Beam);
     break;
   }
 
-  Char_t title[100];
-  sprintf(title,"DeadLayer History (%s)",Beam);
-  TH2D* frame = new TH2D(Beam,title, 10, xmin, xmax, 10, ymin, ymax);
+  sprintf(htitle,"%s%d",Beam,Mode);
+  frame = new TH2D(htitle,title, 10, xmin, xmax, 10, ymin, ymax);
   frame -> SetStats(0);
   frame -> GetXaxis()->SetTitle(xtitle);
   frame -> GetYaxis()->SetTitle(ytitle);
@@ -218,18 +225,33 @@ DlayerMonitor::DlayerPlot(Char_t *Beam, Int_t Mode){
 
   Int_t Color = Beam == "Blue" ? 4 : 94 ;
 
+  // Y-coodinates for Legend
+  Float_t interval=0.15;
+  Float_t ymin=0.15;
+  switch (Mode) {
+  case 30:
+    ymin=0.70;
+    break;
+  case 40:
+    ymin=0.70;
+    break;
+  }
+  Float_t ymax=ymin+interval;
+
   Char_t DATAFILE[256];
   sprintf(DATAFILE,"summary/dLayer_%s_FTP.dat",Beam);
   Int_t ndata = GetData(DATAFILE);
   DrawFrame(Mode, ndata, Beam);
 
-  TLegend * aLegend = new TLegend(0.7,0.15,0.85,0.3);
-  Plot(Mode, ndata, 20, "Flattop", Color, aLegend);
+  // Legend
+  TLegend * aLegend = new TLegend(0.7, ymin, 0.85, ymax);
 
+  // Plot flattop
+  Plot(Mode, ndata, 20, "Flattop", Color, aLegend);
   sprintf(DATAFILE,"summary/dLayer_%s_INJ.dat",Beam);
   Int_t ndata = GetData(DATAFILE);
+  // Plot injection
   Plot(Mode, ndata, 24, "Injection", Color, aLegend);
-
 
   return 0;
 
@@ -237,23 +259,24 @@ DlayerMonitor::DlayerPlot(Char_t *Beam, Int_t Mode){
 
 //
 // Class name  : DlayerMonitor
-// Method name : RunBeam(Int_t Mode)
+// Method name : BlueAndYellowBeams(Int_t Mode, TCanvas *CurC, TPostScript *ps)
 //
 // Description : Execute Blue & Yellow make plot routines
 // Input       : Int_t Mode, TCanvas *CurC, TPostScript *ps
 // Return      : 
 //
 Int_t 
-DlayerMonitor::RunBothBeam(Int_t Mode, TPostScript ps){
+DlayerMonitor::BlueAndYellowBeams(Int_t Mode, TCanvas *CurC, TPostScript *ps){
 
-
-  DlayerPlot("Blue",10);
+  DlayerPlot("Blue",Mode);
   CurC -> Update();
-   
-  ps.NewPage();
-  DlayerPlot("Yellow",10);
-  CurC -> Update();
+  frame->Delete();
 
+  ps->NewPage();
+  DlayerPlot("Yellow",Mode);
+  CurC -> Update();
+  if (Mode!=40) frame->Delete();
+  
   return 0;
     
 }
@@ -272,26 +295,22 @@ Int_t
 DlayerMonitor::DlayerMonitor()
 {
 
-
-    // postscript file
-    Char_t psfile[100];
-    sprintf(psfile,"ps/dLmonitor.ps");
-    TPostScript ps(psfile,112);
-
     // Cambus Setup
     TCanvas *CurC = new TCanvas("CurC","",1);
     CurC -> SetGridy();
 
-  DlayerPlot("Blue",40);
-  CurC -> Update();
-   
-  ps.NewPage();
-  DlayerPlot("Yellow",40);
-  CurC -> Update();
-  //    RunBothBeam(10, ps);
+    // postscript file
+    Char_t psfile[100];
+    sprintf(psfile,"ps/dLmonitor.ps");
+    TPostScript *ps = new TPostScript(psfile,112);
+
+    BlueAndYellowBeams(10, CurC, ps);
+    BlueAndYellowBeams(20, CurC, ps);
+    BlueAndYellowBeams(30, CurC, ps);
+    BlueAndYellowBeams(40, CurC, ps);
 
     cout << "ps file : " << psfile << endl;
-    ps.Close();
+    ps->Close();
     
     return 0;
 
