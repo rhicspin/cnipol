067bcc089e194bebc782d4ac7232f1c77940bd4d
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-05-11 18:30:30 +0000

    added new option to return online number of events.`
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@450 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/OnlinePol.sh
+++ script/OnlinePol.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ExeOnlinePol=1;
+ExeNevents=0;
+UnitMillion=0;
+UnitKiro=0;
 
 #############################################################################
 #                                     Help                                  #
@@ -7,10 +11,13 @@
 help(){
     COMMAND=`basename $0`;
     echo    " "
-    echo    " $COMMAND [-xh][-f <RunID>]"
-    echo    "    : Calculate Online Polarization "
+    echo    " $COMMAND [-xh][-f <RunID>][--nevents][-M]"
+    echo    "    : Calculate Online Polarization & show online records"
     echo    " "
     echo -e "   -f <RunID>     Calculate Online Polarization of <RunID>";
+    echo -e "   --nevents      Print Online # of Events ";
+    echo -e "   -M             Nevents in Million unit ";
+    echo -e "   -k             Nevents in kiro unit ";
     echo -e "   -h | --help    Show this help"
     echo -e "   -x             Show example"
     echo    " "
@@ -30,6 +37,10 @@ ShowExample(){
 
 
 
+
+#############################################################################
+#                               GetOnlinePolariztion                                   #
+#############################################################################
 GetOnlinePolarization(){
 
  TMPDIR=/tmp/cnipol;
@@ -40,12 +51,31 @@ GetOnlinePolarization(){
  $MACRODIR/pvector.pl $RunID
  paw -b $MACRODIR/pvect_simple.kumac &> /dev/null;
  echo -e -n "$RunID" > $TMPDIR/tmp.log
- cat $TMPDIR/tmp.dat | gawk '{printf("%6.1f%5.1f\n",$1,$2)}' 
+ cat $TMPDIR/tmp.dat | gawk '{printf("%6.1f %5.1f\n",$1,$2)}' 
  rm -f $TMPDIR/tmp.dat
  rm -f $TMPDIR/pvect.dat
 
 }
 
+
+
+#############################################################################
+#                                    Main                                   #
+#############################################################################
+OnlineNevents(){
+
+#   echo -e -n "$RunID";
+   ONLINE_LOG=$ONLINEDIR/log/$RunID.log;
+   if [ $UnitMillion -eq 1 ] ; then
+       grep ">>>" $ONLINE_LOG | tail -n 1 | gawk '{printf(" %2d ",$10/1e6)}';
+   elif [ $UnitKiro -eq 1 ] ; then 
+       grep ">>>" $ONLINE_LOG | tail -n 1 | gawk '{printf(" %d ",$10/1e3)}';
+   else 
+       grep ">>>" $ONLINE_LOG | tail -n 1 | gawk '{printf(" %d ",$10)}';
+   fi
+   echo -e -n "\n";
+
+}
 
 #############################################################################
 #                                    Main                                   #
@@ -54,6 +84,9 @@ GetOnlinePolarization(){
 while test $# -ne 0; do
   case "$1" in
   -f) shift ; RunID=$1 ;;
+  --nevents) ExeNevents=1; ExeOnlinePol=0;;
+  -M) UnitMillion=1;;
+  -k) UnitKiro=1;;
   -x) shift ; ShowExample ;;
   -h | --help) help ;;
   *)  echo "Error: Invarid Option $1"
@@ -69,6 +102,11 @@ else
     export RUN=$RunID
 fi
 
-GetOnlinePolarization;
+if [ $ExeOnlinePol -eq 1 ] ; then
+    GetOnlinePolarization;
+fi
 
+if [ $ExeNevents -eq 1 ] ; then
+    OnlineNevents;
+fi
 
