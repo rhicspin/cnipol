6c021035df4b978d44795701d54c9f25523ff1b3
e950 <e950>
2006-05-16 18:14:20 +0000

    implemeted daemon to keep updating online polarization in $DATADIR/OnlinePol.dat
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@460 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/OnlinePol.sh
+++ script/OnlinePol.sh
@@ -4,6 +4,9 @@ ExeOnlinePol=1;
 ExeNevents=0;
 UnitMillion=0;
 UnitKiro=0;
+ONLINE_RUNLIST=$DATADIR/raw_data.list;
+ONLINE_DB=$DATADIR/OnlinePol.dat;
+SLEEP_INTERVAL=1800;
 
 #############################################################################
 #                                     Help                                  #
@@ -11,11 +14,13 @@ UnitKiro=0;
 help(){
     COMMAND=`basename $0`;
     echo    " "
-    echo    " $COMMAND [-xh][-f <RunID>][--nevents][-M]"
+    echo    " $COMMAND [-xh][-f <RunID>][--nevents][--daemon][-s -sleeep <s>][-M]"
     echo    "    : Calculate Online Polarization & show online records"
     echo    " "
     echo -e "   -f <RunID>     Calculate Online Polarization of <RunID>";
     echo -e "   --nevents      Print Online # of Events ";
+    echo -e "   --deamon       Dynamicly update online Pol data file $ONLINE_DB"; 
+    echo -e "   -s --sleep <s> Sleep interval for Daemon mode in sec. [Def]:$SLEEP_INTERVAL";               
     echo -e "   -M             Nevents in Million unit ";
     echo -e "   -k             Nevents in kiro unit ";
     echo -e "   -h | --help    Show this help"
@@ -36,6 +41,19 @@ ShowExample(){
 
 
 
+#############################################################################
+#                                CheckRunID                                 #
+#############################################################################
+CheckRunID(){
+
+    if [ ! $RunID ] ; then
+	echo -e -n "Erorr: RunID is not defined";
+	help;
+    else
+	export RUN=$RunID
+    fi
+
+}
 
 
 #############################################################################
@@ -50,8 +68,8 @@ GetOnlinePolarization(){
 
  $MACRODIR/pvector.pl $RunID
  paw -b $MACRODIR/pvect_simple.kumac &> /dev/null;
- echo -e -n "$RunID" > $TMPOUTDIR/tmp.log
- cat $TMPOUTDIR/tmp.dat | gawk '{printf("%6.1f %5.1f\n",$1,$2)}' 
+ echo -e -n "$RunID" > $TMPOUTDIR/tmp.log;
+ cat $TMPOUTDIR/tmp.dat | gawk '{printf("%7.1f %5.1f\n",$1,$2)}' 
  rm -f $TMPOUTDIR/tmp.dat
  rm -f $TMPOUTDIR/pvect.dat
 
@@ -78,6 +96,35 @@ OnlineNevents(){
 }
 
 #############################################################################
+#                                  ExeDaemon                                #
+#############################################################################
+ExeDaemon(){
+
+  while [ 1 ] ; do 
+
+    NLINE=`wc $ONLINE_RUNLIST | gawk '{print $1}'`
+    for (( i=1; i<=$NLINE; i++ )) ; do
+	RunID=`line.sh $i $ONLINE_RUNLIST`;
+	CheckRunID;
+	grep $RunID $ONLINE_DB;
+	if [ $? -eq 1 ] ; then
+	    echo -e -n "$RunID" | tee -a  $ONLINE_DB ;
+	    GetOnlinePolarization | tee -a $ONLINE_DB ;
+        fi
+
+    done
+
+    echo -e "sleeping $SLEEP_INTERVAL [sec]";
+    sleep $SLEEP_INTERVAL;
+
+  done
+
+}
+	    
+
+
+
+#############################################################################
 #                                    Main                                   #
 #############################################################################
  
@@ -85,6 +132,8 @@ while test $# -ne 0; do
   case "$1" in
   -f) shift ; RunID=$1 ;;
   --nevents) ExeNevents=1; ExeOnlinePol=0;;
+  --daemon) DAEMON=1;;
+  -s | -sleep) shift ; SLEEP_INTERVAL=$1;;
   -M) UnitMillion=1;;
   -k) UnitKiro=1;;
   -x) shift ; ShowExample ;;
@@ -95,18 +144,22 @@ while test $# -ne 0; do
   shift
 done
 
-if [ ! $RunID ] ; then
-    echo -e -n "Erorr: RunID is not defined";
-    help;
-else
-    export RUN=$RunID
-fi
+if [ $DAEMON -eq 1 ] ; then
+    if [ ! -f $ONLINE_DB ] ; then
+	touch $ONLINE_DB
+    fi
+    ExeDaemon ;
+else 
+    
+    CheckRunID;
 
-if [ $ExeOnlinePol -eq 1 ] ; then
-    GetOnlinePolarization;
-fi
+    if [ $ExeOnlinePol -eq 1 ] ; then
+	GetOnlinePolarization;
+    fi
+    
+    if [ $ExeNevents -eq 1 ] ; then
+	OnlineNevents;
+    fi
 
-if [ $ExeNevents -eq 1 ] ; then
-    OnlineNevents;
 fi
 
