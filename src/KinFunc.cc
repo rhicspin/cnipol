68ceb672d7ae5a5445442dadb8ad0240fc163ff9
e950 <e950>
2006-07-26 15:22:47 +0000

    impremented and modified invert kinematics feature in KinFunc.cc. New option -i inverts the kinematics.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@588 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/KinFunc.cc
+++ src/KinFunc.cc
@@ -9,15 +9,25 @@
 #include <math.h>
 #include "Kinema.h"
 
+// defaults
+float x=50;   // [ug/cm*2]
+float dE=400; // [keV]
+const float Precision = 0.1; // [keV]
+
 int
 Usage(char *argv[]){
 
-  cout << "\n Usage:" << argv[0] << "[-w <dwidth>][-d <dE>][-h] [-x]" << endl;
+  cout << "\n Usage:" << argv[0] << "[-w <dwidth>][-e <energy>][-i][-s][-h][-x]" << endl;
   cout << "\n Description: " << endl;
-  cout << "\t calculate error propagation   " << endl;
+  cout << "\t Convert energy deposit <Edep> in [keV] to kinetic energy <Ekin> of 12C " << endl;
+  cout << "\t for given deadlayer thickness <dwidth> [ug/cm^2] in silicon. With invert " << endl;
+  cout << "\t option -i, it calculates invert kinematics <Ekin> -> <Edep> for <dwidth>" << endl;
   cout << "\n Options:" << endl;
-  cout << "\t -w <dwidth>  deadlayer width in [ug/cm^2]" << endl;
-  cout << "\t -d <dE>      energy deposit in [keV]" << endl;
+  cout << "\t -w <dwidth>  deadlayer width in [ug/cm^2]. Def:" << x << endl;
+  cout << "\t -e <energy>  energy deposit (kinetic energy with -i option) in [keV]. Def:" 
+       << dE << endl;
+  cout << "\t -i           calculate invert kinematics. kinetic energy as input." << endl; 
+  cout << "\t -s           show unit in output" << endl;
   cout << "\t -h \t      show this help    " << endl;
   cout << "\t -x \t      show example    " << endl;
   cout << endl;
@@ -29,9 +39,38 @@ int
 Example(char *argv[]){
 
   cout << "\n Exapmle: " << endl;
-  cout << "\t" << argv[0] << " -x" << endl;
+  cout << "  1) Calculate <Ekin> for <Edep>=400 and <dwidth>=55 " << endl;
+  cout << "\t" << argv[0] << " -e 400 -w 55 " << endl << endl;
+  cout << "  2) Calculate <Edep> for <Ekin>=900 and <dwidth>=70 " << endl;
+  cout << "\t" << argv[0] << " -e 900 -w 70 -i " << endl;
   cout << endl;
   exit(0);
+
+}
+
+
+//
+// Class name  : 
+// Method name : CalcInvertKinema
+//
+// Description : Scan dE from 1 [keV] and calcualte Kinetic energy <Ekin0>
+//             : until resulting <Ekin0> is close enough to desired value Ekin.
+// Input       : (float Ekin, float x)
+// Return      : dE
+//
+float 
+CalcInvertKinema(float Ekin, float x){
+
+  float Ekin0 = 0;
+  float dE = 1; // [keV]
+  while (fabs(Ekin-Ekin0)>Precision) {
+
+    Ekin0 = ekin(dE,x);
+    dE += Precision;
+
+  }
+
+  return dE;
 
 }
 
@@ -39,19 +78,24 @@ Example(char *argv[]){
 int 
 main(int argc, char *argv[]) {
 
-  float x, dE;
-  // defaults
-  x=50;   // [ug/cm*2]
-  dE=400; // 400 keV
-  
+  bool InvertKinema = false;
+  bool ShowUnit = false;
+  float Ekin;
+
   int opt;
-   while (EOF != (opt = getopt(argc, argv, "w:d:hx?"))) {
+   while (EOF != (opt = getopt(argc, argv, "w:e:ishx?"))) {
     switch (opt) {
     case 'w':
       x=atof(optarg);
       break;
-    case 'd':
+    case 'e':
       dE=atof(optarg);
+      break;
+    case 'i':
+      InvertKinema = true;
+      break;
+    case 's':
+      ShowUnit = true;
       break;
     case 'x':
       Example(argv);
@@ -64,9 +108,23 @@ main(int argc, char *argv[]) {
 
    }
       
+   if (InvertKinema) {// Search dE close enough to resulting Ekin
+    Ekin = dE; 
+    dE = CalcInvertKinema(Ekin, x);
+   }
+   
+   // Call Main routine
+   Ekin = ekin(dE,x);
 
-  float Tkin = ekin(dE,x);
-  printf("%10.2f%10.2f\n",dE,Tkin);
+   // print results
+   if (ShowUnit) {
+     printf("=====================================================\n");
+     printf("   Ekin[keV]   Edep[keV]  Eloss[keV]  dwidth[ug/cm^2]\n");
+     printf("=====================================================\n");
+   }
+
+   float Eloss = Ekin - dE;
+   printf("%12.2f%12.2f%12.2f%12.2f\n",Ekin,dE,Eloss,x);
 
    return 0;
 
