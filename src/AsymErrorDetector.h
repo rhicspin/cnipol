4e2b9322595830bd8e73cda9ab5fa5583c46836f
e950 <e950>
2006-08-14 20:56:38 +0000

    some minor changes.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@615 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/AsymErrorDetector.h
+++ src/AsymErrorDetector.h
@@ -55,7 +55,9 @@ extern StructBunchCheck bnchchk;
 //=================================================================//
 //                              Misc                               //
 //=================================================================//
-int UnrecognizedAnomaly(int *x, int nx, int *y, int ny, int *z, int &nz);
+int RegisterAnomaly(float x[], int nx, int y[], int ny, int z[], int &nz);
+int RegisterAnomaly(int x[], int nx, int y[], int ny, int z[], int &nz);
+int UnrecognizedAnomaly(int x[], int nx, int y[], int ny, int z[], int &nz);
 void DrawLine(TH1F * h, float x, float y1, int color, int lwidth);
 void DrawLine(TH2F * h, float x0, float x1, float y, int color, int lstyle, int lwidth);
 
