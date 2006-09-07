d0b3c9e886cd617a97419c4e6825de175b7f3377
e950 <e950>
2006-09-07 19:21:43 +0000

    A new feature to Filter runlist based on Filter list. Presently works as FilterRun.pl --src-list <srclist> -f <runlist>.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@640 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/FilterRun.pl
+++ script/FilterRun.pl
@@ -6,6 +6,7 @@
 $ASYMDIR=$ENV{"ASYMDIR"};
 $SHAREDIR=$ENV{"SHAREDIR"};
 $FILTER_DB="$SHAREDIR/db/PhenixPhysicsRun05.db";
+$DEF_FILTERD_DB="./Filtered.list";
 
 #================================================================================#
 #                                Command Line Options                            #
@@ -17,6 +18,7 @@ $DLAYER          = 0;
 $UPDATE_LINK     = 0;
 $PHENIX_DATABASE = 0;
 $PROFILE_RUN     = 0;
+$FILTER_RUN      = 0;
 $KEYWD           ="phenix";
 $DEFAULT_LINK    = 0;
 
@@ -26,6 +28,8 @@ GetOptions(
 	   'phenix' => \$PHENIX_DATABASE ,
 	   'profile' => \$PROFILE_RUN ,
 	   'dlayer' => \$DLAYER , 
+	   'f:s' => \$FILTER_DB ,
+	   'src-list:s' => \$SRC_DB ,
 	   'help' => \$HELP
 );
 
@@ -37,6 +41,11 @@ if($HELP){
 if($PHENIX_DATABASE) {$KEYWD="phenix"; $FILTER_DB="$SHAREDIR/db/PhenixPhysicsRun05.db";};
 if($PROFILE_RUN) {$KEYWD="profile"; $FILTER_DB="$SHAREDIR/db/ProfileRun05.db";};
 if($DEFAULT_LINK) {$KEYWD="all"; $UPDATE_LINK=1;};
+if($SRC_DB) { 
+    open(FILE,">$DEF_FILTERD_DB") || die ; 
+    Filter();
+    print "Output: $DEF_FILTERD_DB\n";
+}
 if($DLAYER){DlayerMode();};
 
 
@@ -46,19 +55,24 @@ if($DLAYER){DlayerMode();};
 sub help(){
 
     print "\n";
-    print " Usage:\n  $0 -h [--dlayer][ -f <runlist>][--update-link][--def-link]\n\n";
-    print "    Filter runs based on the runlist\n\n";
-    print "\t -f <runlist>  run list file [def]:$FILTER_DB \n";
-    print "\t --dlayer      Process deadlayer dabase\n";
-    print "\t --phenix      Filter for PHENIX physics stores\n";
-    print "\t --profile     Filter for profile runs\n";
-    print "\t --update-link Redirect link to new one\n";
-    print "\t --def-link    Redirect link to default (all)\n";
+    print " Usage:\n  $0 [-h][--dlayer][ -f <runlist>][--update-link][--def-link]\n\n";
+    print "              [--src-list <srclist>]"; 
+    print "    Filter runs based on the runlist. ";
+    print "    dlayer SRC datafile = summary/dLayer_Blue(Yellow)_FTP(INJ)_all.dat.\n\n";
+    print "\t -f <runlist>   base run list file [def]:$FILTER_DB \n";
+    print "\t --src-list <srclist>  src run list file to be filtered \n";
+    print "\t --dlayer       Process deadlayer dabase\n";
+    print "\t --phenix       Filter for PHENIX physics stores\n";
+    print "\t --profile      Filter for profile runs\n";
+    print "\t --update-link  Redirect link to new one\n";
+    print "\t --def-link     Redirect link to default (all)\n";
     print "\t -h --help  Show this help";
     print "\n\n";
     print "    ex.1) $0 -f runlist.db --dlayer \n\n";
     print "    ex.2) Redirect link to default (all) files:\n";
     print "          $0 --dlayer --def-link \n\n";
+    print "    ex.3) Filter <srclist> based on <runlist>\n";
+    print "          $0 --src-list <srclist> -f <runlist>";
     print "\n\n";
     exit(0);
 
