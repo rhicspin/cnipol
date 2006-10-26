facd15a985f17b42b81a3b0300401656bbc6224d
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2006-10-26 19:48:49 +0000

    implemented anal.anomaly.bad_bunch_rate and bad_st_rate.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@660 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/Asym.h
+++ src/Asym.h
@@ -127,8 +127,10 @@ typedef struct {
 typedef struct {
   int nstrip;
   int st[NSTRIP];
+  float bad_st_rate;
   int nbunch;
   int bunch[NBUNCH];
+  float bad_bunch_rate;
 } StructAnomaly;
 
 typedef struct {
@@ -148,8 +150,8 @@ typedef struct {
   float P[2];
   float P_sigma_ratio[2];
   float P_sigma_ratio_norm[2];
-  float energy_slope[2];
-  StructSinPhi sinphi[2];   // [0]: regular,   [1]: alternative sigma cut
+  float energy_slope[2];    // Slope for energy spectrum (detectors sum) [0]:slope [1]:error
+  StructSinPhi sinphi[2];   // [0]: regular,  [1]: alternative sigma cut
   StructAnomaly anomaly;
   StructUnrecognized unrecog;
 } StructAnalysis;
