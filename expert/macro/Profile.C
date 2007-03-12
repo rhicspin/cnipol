f8021dd2882ef9a6f97166b0962680793f971454
e950 <e950>
2007-03-12 19:57:48 +0000

    Impremented rate_vs_P correlations.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@788 cd24ecce-3d09-47a0-95ec-c030155e877a

--- expert/macro/Profile.C
+++ expert/macro/Profile.C
@@ -22,6 +22,11 @@ struct StructProfile {
   Float_t Chi2NDF[N];
   Int_t Threshold;
   Int_t Color;
+  struct StructFitResults{
+    Float_t Const;
+    Float_t Mean;
+    Float_t Sigma;
+  } pol, R;
 } yellow[2], Blue;
 
 yellow[0].Color = yellow[1].Color = 95;
@@ -29,7 +34,7 @@ Blue.Color = 4;
 
 yellow[0].beam_center  = 161.3;
 yellow[1].beam_center  = 160.4;
-Blue.beam_center       = 118.9;
+Blue.beam_center       = 118.87;
 
 yellow[0].max_rate     = 0.05307;
 yellow[1].max_rate     = 0.03977;
@@ -77,10 +82,11 @@ Int_t GetData(Char_t *filename, StructProfile &profile)
 
 
 Float_t 
-PolarizationProfile(StructProfile profile, Int_t ndata, Int_t Color, Int_t DoFit){
+PolarizationProfile(StructProfile &profile, Int_t ndata, Int_t Color, Int_t FitMode){
 
-  Int_t MStyle = DoFit ? 31 : 20;
-  Color = DoFit ? 1 : Color;
+  Float_t Chi2DOF = 0;
+  Int_t MStyle = FitMode ? 31 : 20;
+  Color = FitMode ? 1 : Color;
 
   TGraphErrors * Pol = new TGraphErrors(ndata, profile.xpos_norm, profile.P, profile.ex, profile.dP);
   Pol -> SetTitle(profile.hname);
@@ -89,20 +95,102 @@ PolarizationProfile(StructProfile profile, Int_t ndata, Int_t Color, Int_t DoFit
   Pol -> Draw("P");
 
   TF1 * f1 = new TF1("f1","pol0");
+  TF1 * g1 = new TF1("g1","gaus");
+  g1->FixParameter(1,0); // mean=0. force symmetric profile
   // if Dofit, then do polinomial-0 fit
-  if (DoFit) Pol->Fit(f1,"same");
+  if (FitMode==1) {
+    Pol->Fit(f1,"same");
+    Chi2DOF=f1->GetChisquare()/f1->GetNDF();
+  } else if (FitMode==2) {
+    Pol->Fit(g1,"same");
+    Chi2DOF=g1->GetChisquare()/g1->GetNDF();
+    profile.pol.Const = g1->GetParameter(0);
+    profile.pol.Mean  = g1->GetParameter(1);
+    profile.pol.Sigma = g1->GetParameter(2);
+  }
+
+  return Chi2DOF ;
+
+}
 
 
-  return DoFit ? f1->GetChisquare()/f1->GetNDF() : 0 ;
+Double_t rate_vs_xpos(Double_t *x, Double_t par)
+{
+  return par[3]*sqrt(2*log(par[1]/x));
+}
+
+//
+// Class name  : 
+// Method name : 
+//
+// Description : 
+//             : 
+// Input       : 
+// Return      : 
+//
+Int_t 
+PolarizationVsRateCorrelation(StructProfile profile, TPostScript * ps){
+
+  Float_t xmin=0.1, xmax=1;
+  Char_t title[100], htitle[100];
+  sprintf(title,"%s",profile.hname);
+  TCanvas * C = new TCanvas("C","",1100, 850);
+  ps->NewPage();
+
+
+  C->Divide(1,3);
+
+  C->cd(1);
+  TF1 * rate_vs_xpos = new TF1("rate_vs_xpos","[1]*sqrt(2*log([0]/x))", xmin, xmax);
+  sprintf(htitle,"%s : Rate vs. Target Position", title);
+  rate_vs_xpos -> SetTitle(htitle);
+  rate_vs_xpos -> SetParameters(profile.R.Const, profile.R.Sigma, profile.pol.Const, profile.pol.Sigma);
+  rate_vs_xpos -> SetLineColor(profile.Color);
+  rate_vs_xpos -> Draw("LA");
+  rate_vs_xpos -> GetXaxis()->SetTitle("Rate relative to center");
+  rate_vs_xpos -> GetYaxis()->SetTitle("Target position from center [mm]");
+
+  C->cd(2);
+  TF1 * corr_abs = new TF1("corr_abs","[2]*exp(-[1]*[1]/[3]/[3]*log([0]/x))", 0.01, 1);
+  sprintf(htitle,"%s : Rate vs. Polarization", title);
+  corr_abs -> SetTitle(htitle);
+  corr_abs -> SetParameters(profile.R.Const, profile.R.Sigma, profile.pol.Const, profile.pol.Sigma);
+  corr_abs -> SetLineColor(profile.Color);
+  corr_abs -> Draw("LA");
+  corr_abs -> GetXaxis()->SetTitle("Rate relative to center");
+  corr_abs -> GetYaxis()->SetTitle("P(rate)[%]");
+
+  C->cd(3);
+  TF1 * corr_rel = new TF1("corr_rel","(([2]-[2]*exp(-[1]*[1]/[3]/[3]*log([0]/x)))/[2])*100", 0.01, 1);
+  sprintf(htitle,"%s : Rate vs. Polarization drop from center (Relative)", title);
+  corr_rel -> SetTitle(htitle);
+  corr_rel -> SetParameters(profile.R.Const, profile.R.Sigma, profile.pol.Const, profile.pol.Sigma);
+  corr_rel -> SetLineColor(profile.Color);
+  corr_rel -> Draw("LA");
+  corr_rel -> GetXaxis()->SetTitle("Rate relative to center");
+  corr_rel -> GetYaxis()->SetTitle("{P(0)-P(rate)}/P(0) [%]");
+
+  C->Update();
+
+  return 0;
 
 }
 
 
 
-
-
+//
+// Class name  : 
+// Method name : MakePlot(StructProfile &profile, TPostScript * ps, Int_t k, Int_t FitMode)
+//
+// Description : Make Plots Polarization and Beam Intensity Profiles
+//             : FitMode : 0  - no fit
+//             : FitMode : 1  - linear polarization profile fit
+//             : FitMode : 2  - gaussian polarization profile fit
+// Input       : (StructProfile &profile, TPostScript * ps, Int_t k, Int_t FitMode)
+// Return      :  StructProfile &profile (rate_norm calculated here)
+//
 Float_t 
-MakePlot(StructProfile &profile, TPostScript * ps, Int_t k)
+MakePlot(StructProfile &profile, TPostScript * ps, Int_t k, Int_t FitMode)
 {
 
   Float_t Chi2NDF=0;
@@ -124,7 +212,8 @@ MakePlot(StructProfile &profile, TPostScript * ps, Int_t k)
   frame->Draw();
 
   PolarizationProfile(profile, profile.ndata, profile.Color, 0);    C->Update();
-  if (k!=-1) Chi2NDF = PolarizationProfile(profile, profile.ndata-k, 2, 1);  C->Update();
+
+  if (FitMode>=1) Chi2NDF = PolarizationProfile(profile, profile.ndata-k, 2, FitMode);  C->Update();
 
   C->cd(2);
   TH2F * frame = new TH2F("frame","",100, xmin, xmax, 100, 0, 1.1);
@@ -133,16 +222,16 @@ MakePlot(StructProfile &profile, TPostScript * ps, Int_t k)
   frame -> GetYaxis()->SetTitle("Good Carbon Rate in Banana/WCM_sum");
 
   TF1 * g = new TF1("g","gaus");
-  if (k==-1){
+  if (FitMode==0){
     TGraph * Norm = new TGraph(profile.ndata, profile.xpos_norm, profile.rate);
+    Norm->GetXaxis()->SetTitle("Target Position [mm]");
+    Norm->GetYaxis()->SetTitle("Carbon Rate/WCM_sum");
     Norm->SetMarkerColor(profile.Color);
     Norm->SetMarkerStyle(20);
     Norm->Fit(g);
     Norm->Draw("AP");
     profile.max_rate = g->GetParameter(0);
-    cout << "Max rate=" << g->GetParameter(0) << endl;
     for (Int_t i=0; i<profile.ndata; i++) profile.rate_norm[i] = profile.rate[i] / profile.max_rate;
-
   } else {
     frame->Draw();
     TGraph * Rate = new TGraph(profile.ndata, profile.xpos_norm, profile.rate_norm);
@@ -151,6 +240,9 @@ MakePlot(StructProfile &profile, TPostScript * ps, Int_t k)
     Rate -> SetMarkerStyle(20);
     Rate -> Draw("P");
     Rate -> Fit(g);
+    profile.R.Const = g->GetParameter(0);
+    profile.R.Mean = g->GetParameter(1);
+    profile.R.Sigma = g->GetParameter(2);
 
     Float_t hline=profile.rate_norm[profile.ndata-k-1];
     TLine * t = new TLine(xmin, hline, xmax, hline);
@@ -186,16 +278,7 @@ Int_t PrintResults(StructProfile profile){
 
 }
 
-
-Int_t DataLoop(StructProfile profile, TPostScript * ps){
-
-
-
-  MakePlot(profile, ps, -1); 
-  for (Int_t k=0; k<profile.ndata-1; k++)  profile.Chi2NDF[profile.ndata-1-k] = MakePlot(profile, ps, k);
-
-  PrintResults(profile);
-
+Int_t Chi2Distribution(StructProfile profile, TPostScript * ps){
 
   TCanvas * C0 = new TCanvas();
 
@@ -220,6 +303,48 @@ Int_t DataLoop(StructProfile profile, TPostScript * ps){
 
   PolDist->Fit("gaus");
   C0->Update(); 
+
+
+  return 0;
+}
+
+
+
+Int_t DataLoop(StructProfile profile, TPostScript * ps){
+
+  // Make polarization and beam intensity profile plots without drawing fit
+  MakePlot(profile, ps, 0, 0); 
+
+  // Make polarization and beam intensity profile plots without drawing fit
+  MakePlot(profile, ps, 0, -1); 
+
+  // Linear fit polarization profile
+  MakePlot(profile, ps, 0, 1);
+
+  // Gaussian fit polarization profile
+  MakePlot(profile, ps, 0, 2);
+
+  PolarizationVsRateCorrelation(profile, ps);
+
+
+
+
+  //  =========================================================================//
+  //                Chi2-fits on selected data points                          //
+  //  =========================================================================//
+  /*
+  // Profile linear fit excluding "k"th data and after 
+  for (Int_t k=0; k<profile.ndata-1; k++)  profile.Chi2NDF[profile.ndata-1-k] = MakePlot(profile, ps, k, 1);
+  Chi2Distribution(profile, ps);
+
+  // Profile linear fit excluding "k"th data and after 
+  for (Int_t k=0; k<profile.ndata-3; k++)  profile.Chi2NDF[profile.ndata-1-k] = MakePlot(profile, ps, k, 2);
+  Chi2Distribution(profile, ps);
+  */
+
+
+  PrintResults(profile);
+
 
 
   return;
@@ -273,9 +398,9 @@ Int_t Profile()
   Blue.ndata = GetData(filename, Blue);
   
 
-  //  ProfileControl(yellow[0]); // Yellow - 7131
+  ProfileControl(yellow[0]); // Yellow - 7131
   ProfileControl(yellow[1]); // Yellow - 7151
-  //  ProfileControl(Blue);      // Blue   - 7151
+  ProfileControl(Blue);      // Blue   - 7151
 
 
 
