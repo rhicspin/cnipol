870739e9443f89fd5719102524bb01807b829554
e950 <e950@cd24ecce-3d09-47a0-95ec-c030155e877a>
2008-01-31 18:44:44 +0000

    A script to install cni library files to SHAREDIR.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@859 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ cnilib/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+if [ ! -d $CNILIB ] ; then
+    echo "Create $CNILIB" ;
+    mkdir -p $CNILIB;
+fi
+
+if [ ! -d $SHAREDIR ] ; then
+    echo "Create $SHAREDIR" ;
+    mkdir -p $SHAREDIR;
+fi
+
+
+# Install Calibration Data Files
+if [ ! -d $CALIBDIR ] ; then
+    echo "Create $SHAREDIR" ;
+    mkdir -p $CALIBDIR;
+fi
+
+tar xvzf Run06_calib.tgz -C $CALIBDIR
+
+# Install Configuration Files
+if [ ! -d $CONFDIR ] ; then
+    echo "Create $CONFDIR" ;
+    mkdir -p $CONFDIR;
+fi
+
+tar xvzf Run06_config.tgz -C $CONFDIR
+
+
+# Install DeadLayer Files
+if [ ! -d $SHAREDIR/dlayer ] ; then
+    echo "Create $SHAREDIR/dlayer" ;
+    mkdir -p $SHAREDIR/dlayer;
+fi
+
+tar xvzf Run06_dlayer.tgz -C $SHAREDIR/dlayer
+
+echo "Installation completed."
+
+
