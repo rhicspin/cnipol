0bf20a0087343fe3aaf775089b5fc2c5950b9f6f
e950 <e950>
2008-02-07 16:16:43 +0000

    The --daemon option is now called --online. Also new option --update will do the single shot of online polarization table update without iteration.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@876 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/OnlinePol.sh
+++ script/OnlinePol.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ExeDlayerAverage=0;
 ExeOnlinePol=1;
 ExeNevents=0;
@@ -11,6 +10,7 @@ ONLINE_RUNLIST=$DATADIR/raw_data.list;
 ONLINE_DB=$DATADIR/OnlinePol.dat;
 SLEEP_INTERVAL=1800;
 DAEMON=0;
+UPDATE=0;
 A_NCorrection=0;
 
 #############################################################################
@@ -19,17 +19,18 @@ A_NCorrection=0;
 help(){
     COMMAND=`basename $0`;
     echo    " "
-    echo    " $COMMAND [-xh][-f <RunID>][--nevents][--daemon][-s -sleeep <s>][-M][--A_NCorrection]"
-    echo    "          [--dlayer-average]"
+    echo    " $COMMAND [-xh][-f <RunID>][--nevents][--online][-s -sleeep <s>][-M]";
+    echo    "          [--dlayer-average][--update]"
     echo    "    : Calculate Online Polarization & show online records / make deadlyaer average in online config."
     echo    " "
     echo -e "   -f <RunID>       Calculate Online Polarization of <RunID>";
+    echo -e "   --update         Update the online Pol data file $ONLINE_DB";
+    echo -e "   --online         Dynamicly update online Pol data file $ONLINE_DB"; 
+    echo -e "   -s --sleep <s>   Sleep interval for Online mode in sec. [Def]:$SLEEP_INTERVAL";               
     echo -e "   --nevents        Print Online # of Events ";
-    echo -e "   --deamon         Dynamicly update online Pol data file $ONLINE_DB"; 
-    echo -e "   -s --sleep <s>   Sleep interval for Daemon mode in sec. [Def]:$SLEEP_INTERVAL";               
-    echo -e "   -M               Nevents in Million unit ";
-    echo -e "   -k               Nevents in kiro unit ";
-    echo -e "   --A_NCorrection  A_N correction for before Fill#6884";
+    echo -e "    -M              Nevents in Million unit ";
+    echo -e "    -k              Nevents in kiro unit ";
+#    echo -e "   --A_NCorrection  A_N correction for before Fill#6884";
     echo -e "   --dlayer-average Make deadlyaer average for online config files in $ONLINE_CONFIGDIR";
     echo -e "   -h | --help      Show this help"
     echo -e "   -x               Show example"
@@ -40,19 +41,33 @@ help(){
  
 ShowExample(){
  
-    COMMAND=`basename echo`;    
+    COMMAND=`basename $0`;    
     $0    " ";
     echo    " 1. Get online polarization for RunID=7759.002";
-    echo    " $COMMAND -f 7759.002";
     echo    " ";
-    echo    " 2. Running in daemon mode";
-    echo    " $COMMAND --daemon";
+    echo    "       $COMMAND -f 7759.002";
     echo    " ";
-    echo    " 3. A_N Correction for Fill<6884 (only for flattops). ";
-    echo    " $COMMAND -f 6883 --A_NCorrection";
+    echo    " 2. Update online polarization summary table: ";
+    echo    "    $ONLINE_DB";
     echo    " ";
-    echo    " 4. Create deadlayer summary list in $ONLINE_CONFIGDIR/dLayerAverage.list";
-    echo    " $COMMAND --dlayer-average | tee $ONLINE_CONFIGDIR/dLayerAverage.list";
+    echo    "      $COMMAND --update";
+    echo    " ";
+    echo    " 3. Running in online mode. Makes online polarization summary table: ";
+    echo    "    $ONLINE_DB";
+    echo    " ";
+    echo    "      $COMMAND --online";
+    echo    " ";
+    echo    " 4. Get number of events of <RunID> in the unit of Mevents";
+    echo    " ";
+    echo    "      $COMMAND -f <RunID> --nevents -M";
+    echo    " ";
+#    echo    " 3. A_N Correction for Fill<6884 (only for flattops). ";
+#    echo    " ";
+#    echo    "      $COMMAND -f 6883 --A_NCorrection";
+#    echo    " ";
+    echo    " 5. Create deadlayer summary list in $ONLINE_CONFIGDIR/dLayerAverage.list";
+    echo    " ";
+    echo    "      $COMMAND --dlayer-average | tee $ONLINE_CONFIGDIR/dLayerAverage.list";
     echo    " ";
     exit;
 
@@ -155,17 +170,11 @@ OnlineNevents(){
 }
 
 #############################################################################
-#                                  ExeDaemon                                #
+#                                  UpdateTable                              #
 #############################################################################
-ExeDaemon(){
-
-if [ ! -f $ONLINE_DB ] ; then
-    touch $ONLINE_DB;
-fi
+UpdateTable(){
 
     
-  while [ 1 ] ; do 
-
     NLINE=`wc $ONLINE_RUNLIST | gawk '{print $1}'`
     for (( i=1; i<=$NLINE; i++ )) ; do
 	RunID=`line.sh $i $ONLINE_RUNLIST`;
@@ -178,13 +187,30 @@ fi
 
     done
 
-    echo -e "sleeping $SLEEP_INTERVAL [sec]";
-    sleep $SLEEP_INTERVAL;
+}
+	    
+
+#############################################################################
+#                                  ExeDaemon                                #
+#############################################################################
+ExeDaemon(){
+
+if [ ! -f $ONLINE_DB ] ; then
+    touch $ONLINE_DB;
+fi
+
+    
+  while [ 1 ] ; do 
+
+      UpdateTable;
+
+      echo -e "sleeping $SLEEP_INTERVAL [sec]";
+      sleep $SLEEP_INTERVAL;
 
   done
 
 }
-	    
+
 
 
 
@@ -198,7 +224,8 @@ while test $# -ne 0; do
   --dlayer-average) ExeDlayerAverage=1;;
   --A_NCorrection) A_NCorrection=1;;
   --nevents) ExeNevents=1; ExeOnlinePol=0;;
-  --daemon) DAEMON=1;;
+  --update) UPDATE=1;;
+  --online) DAEMON=1;UPDATE=1;;
   -s | -sleep) shift ; SLEEP_INTERVAL=$1;;
   -M) UnitMillion=1;;
   -k) UnitKiro=1;;
@@ -217,11 +244,15 @@ if [ $ExeDlayerAverage -eq 1 ] ; then
 fi
 
 
-if [ $DAEMON -eq 1 ] ; then
+if [ $UPDATE -eq 1 ] ; then
     if [ ! -f $ONLINE_DB ] ; then
 	touch $ONLINE_DB
     fi
-    ExeDaemon ;
+    if [ $DAEMON -eq 1 ] ; then
+	ExeDaemon ;
+    else
+	UpdateTable ;
+    fi
 else 
     
     CheckRunID;
