e80d3fd0d46c748c640d42a8778d6086730457ec
e950 <e950>
2009-03-05 19:43:16 +0000

    Make summary of FTP Offline data, instead of all.
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@988 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/MakeSummary.sh
+++ script/MakeSummary.sh
@@ -8,11 +8,11 @@ LinkFile(){
 cd $SUMMARYDIR
 
 if [ ! -f  $FILE\_Blue.dat ] ; then
-    ln -s $FILE\_Blue_all.dat $FILE\_Blue.dat;
+    ln -s $FILE\_Blue_$MODE.dat $FILE\_Blue.dat;
 fi
 
 if [ ! -f  $FILE\_Yellow.dat ] ; then
-    ln -s $FILE\_Yellow_all.dat $FILE\_Yellow.dat;
+    ln -s $FILE\_Yellow_$MODE.dat $FILE\_Yellow.dat;
 fi
 
 }
@@ -33,9 +33,9 @@ fi
 
 
 LINK_FILE(){
-    FILE="OfflinePol";
+    FILE="OfflinePol"; MODE="FTP";
     LinkFile;
-    FILE="ErrorDet";
+    FILE="ErrorDet"; MODE="all";
     LinkFile;
     FILE="dLayer"; MODE="FTP";
     LinkFileDlayer;
@@ -45,16 +45,30 @@ LINK_FILE(){
 
 
 MAIN(){
+
 cd $ASYMDIR
+
+# Online Summary
 mkDB.sh --online --yellow | tee summary/Online_Yellow.dat ; mkDB.sh --online --blue | tee summary/Online_Blue.dat
+
+# DeadLayer Summary
 dLayerAve.sh -f .cnipol_daemon_run.list --distribute | tee /tmp/cnipol/dLayerAve.log
 root -b -q $MACRODIR/DlayerMonitor.C
+
+#Offline Polarization Summary
 mkDB.sh --blue > summary/OfflinePol_Blue_all.dat ; mkDB.sh --yellow > summary/OfflinePol_Yellow_all.dat
+head -4 summary/OfflinePol_Blue_all.dat  > summary/OfflinePol_Blue_FTP.dat 
+grep " 249 " summary/OfflinePol_Blue_all.dat >> summary/OfflinePol_Blue_FTP.dat
+head -4 summary/OfflinePol_Yellow_all.dat > summary/OfflinePol_Yellow_FTP.dat
+grep " 249 " summary/OfflinePol_Yellow_all.dat >> summary/OfflinePol_Yellow_FTP.dat
 root -b -q $MACRODIR/OfflinePol.C
+
+# Error Detector Summary
 mkDB.sh --error-detector --blue > summary/ErrorDet_Blue_all.dat ; mkDB.sh --error-detector --yellow > summary/ErrorDet_Yellow_all.dat
 root -b /usr/local/bin/macro/ErrorDetector.C
 
 }
+
 
 
 
