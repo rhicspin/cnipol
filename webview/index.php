8c073ceafa2d7f7ad311b1f14fd511b3ee51e521
Dmitri Smirnov <dsmirnov@bnl.gov>
2010-11-10 22:07:40 +0000

    New templates
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1048 cd24ecce-3d09-47a0-95ec-c030155e877a

--- webview/index.php
+++ webview/index.php
@@ -7,55 +7,92 @@ setlocale(LC_TIME, 'en');
 putenv("TZ=America/New_York");
 
 
-if (isset($_GET['runid'])) {
+// Run details view
+if (isset($_GET['runid']) && !isset($_GET['chanid'])) {
+
+   $gRunId = $_GET['runid'];
 
    // check for correct and existing  runid
-
-	$gRunId = $_GET['runid'];
-
-   // Read information about this run
-   include($gRunId."/runconfig.php"); 
-   include("./config_plots.php"); 
-   include_once("PlotHelper.php"); 
-
-   $dir = "./$gRunId/images/";
-   $gP = new PlotHelper($dir, $plots_set1);
-
-	// Get all images in this folder
-   $dir = opendir($gRunId."/images/");
-	$imgs = array();
-   
-   while (false !== ($file = readdir($dir))) {
-      if ( preg_match("/^.+\.png$/", $file) ) {
-         //echo "$file<br>\n";
-			$imgs[] = $file;
-      }
+   if (!is_dir($gRunId) || !file_exists("$gRunId/runconfig.php")) {
+      print "No results for $gRunId\n";
+      exit;
    }
 
-   include("runinfo.html");
+   // Read information about this run
+   include("$gRunId/runconfig.php");
+   //include("./config_plots.php");
+   include_once("PlotHelper.php");
 
-	exit;
+   $dir = "./$gRunId/images/";
+   $gP = new PlotHelper($dir);
+
+   if (ereg("[0-9]{3,}\.[0-9]{3}", $gRunId) && $rc['CMODE'] == 0) {
+      include("runinfo.html");
+   } elseif ($rc['CMODE'] == 1) {
+      include("runinfo_calib.html");
+   } else {
+      print "Problem with $gRunId\n";
+   }
+
+   exit;
 }
 
-//Form default output
 
+// Channel details view
+if (isset($_GET['runid']) && isset($_GET['chanid'])) {
+
+   $gRunId  = $_GET['runid'];
+   $gChanId = $_GET['chanid'];
+
+   // check for correct and existing  runid
+   if (!is_dir($gRunId) || !file_exists("$gRunId/runconfig.php")) {
+      print "No results for $gRunId\n";
+      exit;
+   }
+
+   // Read information about this run
+   include("$gRunId/runconfig.php");
+   //include("./config_plots.php");
+   include_once("PlotHelper.php");
+
+   $dir = "./$gRunId/images/";
+   $gP = new PlotHelper($dir);
+
+   if (ereg("[0-9]{3,}\.[0-9]{3}", $gRunId) && $rc['CMODE'] == 0) {
+      include("chaninfo.html");
+   } elseif ($rc['CMODE'] == 1) {
+      include("chaninfo_calib.html");
+   } else {
+      print "Problem with $gRunId\n";
+   }
+
+   exit;
+}
+
+
+//Form default output
 $dir = opendir('.');
 
 if (!$dir) exit("ERROR\n");
 
+$rs = array();
+
 while (false !== ($file = readdir($dir))) {
 
-   if (ereg("[0-9]{3,}\.[0-9]{3}", $file) && is_dir($file)) {
+   if (is_dir($file) && file_exists("$file/runconfig.php")) {
+      //if (ereg("[0-9]{3,}\.[0-9]{3}", $file) && is_dir($file))
 
       //echo "$file ".$rs['dirsize']."<br>\n";
-      //include($file."/runconfig.php"); 
-      $gRunId = $file;
+      //include("$file/runconfig.php");
+      $rs[] = $file;
       //$rs[$gRunId]['NumChannels'] = $rc['data']['NumChannels'];
-      $rs[$gRunId]['dirsize'] = exec( "du -h -s $file" );
+      //$rs[$gRunId]['dirsize'] = exec( "du -h -s $file" );
    }
 }
 
 closedir($dir);
+
+sort($rs);
 
 include("runinfo_index.html");
 
