632ccd5f4de432b7548649c57d0715642a07c8b9
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2010-12-15 20:34:25 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1101 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ online/rhicpol/rpolutil.h
@@ -0,0 +1,73 @@
+#ifndef POLUTIL_H
+#define POLUTIL_H
+
+#include "rpoldata.h"
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+//	Config functions (rpolutil.c)
+int readConfig(char *fname, int update);
+int CheckConfig(void);
+void CreateLookup(float tshift);
+
+//	Data file write (rpolutil.c)
+int polWrite(recordHeaderStruct *header, long *data);
+void closeDataFile(char * comment);
+int openDataFile(char *fname, char *comment, int noAdo);
+void writeSubrun(int N);
+
+//	Camac & hardware functions (rpolutil.c)
+void camacClose(void);
+int camacOpen(void);
+void setInhibit(void);
+void resetInhibit(void);
+int setOutReg(void);
+void pulseAllProgs(void);
+int initWFDs(void);
+void fastInitWFDs(int clr_hist);
+void initScalers(void);
+int getNumberOfEvents(void);
+int getEvents(int Number);
+void readWFD(void);
+void readMemory(void);
+void readScalers(void);
+
+//	General functions (rpolutil.c)
+void nsleep(double time);
+long long totalSum(long *data, int len);
+void setAlarm(float mTime);
+void clearAlarm(void);
+void alarmHandler(int sig);
+void polexit(void);
+char * stat2str(int stat);
+
+//	Jet functions	(rpolutil.c)
+void setOutInhibit(void);
+void resetOutInhibit(void);
+void clearVetoFlipFlop(void);
+void getJetStatus(unsigned short * data);
+int testJetVeto(void);
+static char * getJetStatusString(void);
+void writeJetStatus(void);
+int getJetBits(void);
+int testCarbTarg(void);
+void writeCarbTarg(void);
+
+//	CDEV functions	(rpolutilp.cpp)
+void getJetPosition(void);
+int getTargetMovementInfo(long **data);
+void getAdoInfo(void);
+void getWcmInfo(void);
+void getCarbTarg(carbTargStat * targstat);
+void GetTargetEncodings(long *res);
+void UpdateProgress(int evDone, int rate, double ts);
+void UpdateStatus(void);
+void UpdateMessage(char *message);
+void ProgV124(int ring);
+#ifdef __cplusplus
+    }
+#endif
+
+#endif
