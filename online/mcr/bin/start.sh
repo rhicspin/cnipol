8506ed7e11aa048b11ea94804c6804071a9883be
Dmitri Smirnov <dsmirnov@bnl.gov>
2010-12-18 22:18:10 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1102 cd24ecce-3d09-47a0-95ec-c030155e877a

--- online/mcr/bin/start.sh
+++ online/mcr/bin/start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 cd /usr/local/polarim/bin
-./rpoldaemon.sh -D >>/dev/null &
-exit $status 
+#./rpoldaemon.sh -U >>/dev/null &
+#exit $status 
+
+HOSTNAME=`hostname -s`
+
+if [ $HOSTNAME = "bluepc" ]; then
+	#echo $HOSTNAME
+	./rpoldaemon.sh -U >>/dev/null &
+	exit $status 
+else
+	if [ $HOSTNAME = "insideip12" ]; then
+		#echo $HOSTNAME
+		./rpoldaemon.sh -D >>/dev/null &
+		exit $status 
+	else
+		echo "Host not recognized. Start script on either bluepc or insideip12"
+	fi
+fi
