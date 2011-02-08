c61a7908c0eec4e7d88f1b7a217c1d3ef3874998
Dmitri Smirnov <dsmirnov@bnl.gov>
2011-02-08 00:21:56 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1135 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ online/rpoldaemon/globals.cpp
@@ -0,0 +1,7 @@
+
+#include "rhicpol.h"
+
+// dsmirnov thinks this is wrong as it does not agree with the previous run idexes
+//char polCDEVName[4][20] = {"polarimeter.blu1", "polarimeter.blu2", "polarimeter.yel1", "polarimeter.yel2"};
+// this should be better
+char polCDEVName[4][20] = {"polarimeter.blu1", "polarimeter.yel1", "polarimeter.blu2", "polarimeter.yel2"};
