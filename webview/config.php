597923dec7a82d74e93ee118ebfce5d8ae693675
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-02-18 23:35:31 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1144 cd24ecce-3d09-47a0-95ec-c030155e877a

--- webview/config.php
+++ webview/config.php
@@ -2,6 +2,7 @@
 
 define("DATA_DIR", "/usr/local/polarim/root");
 
+$POLARIMETER_ID = array(0 => "B1U", 1 => "Y1D", 2 => "B2D", 3 => "Y2U");
 $RHIC_BEAM   = array("1" => "Yellow", "2" => "Blue");
 $RHIC_STREAM = array("1" => "Upstream", "2" => "Downstream");
 
