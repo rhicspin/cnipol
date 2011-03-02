cab5a067560f68a5db90b45c8ccdf3be7e47c24f
Dmitri Smirnov <dsmirnov@bnl.gov>
2011-03-02 23:23:14 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1169 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/setup_rlnxsp.sh
+++ script/setup_rlnxsp.sh
@@ -2,6 +2,8 @@
 
 echo "Setting environmet variables:"
 
+source /afs/rhic.bnl.gov/opt/phenix/root-5.27.04/bin/thisroot.sh
+
 # ROOT variables
 if [ -z "$ROOTSYS" ]; then
 	source /usr/local/cern/root/bin/thisroot.sh
@@ -12,7 +14,7 @@ fi
 
 # CERNLIB variables
 if [ -z "$CERNLIB_DIR" ]; then
-	export CERNLIB_DIR=/usr/local/cern/2006b
+	export CERNLIB_DIR=/cern/2006b
 	export PATH=$PATH:$CERNLIB_DIR/bin
 	echo "\$CERNLIB_DIR=$CERNLIB_DIR"
 else
@@ -24,14 +26,14 @@ fi
 
 # CNIPOL variables
 if [ -z "$CNIPOL_DIR" ]; then
-	export CNIPOL_DIR=~/cnipol/cnipol/trunk
+	export CNIPOL_DIR=~/cnipol
 	#export CNIPOL_DIR=/usr/local/cnipol
 	export PATH=$PATH:$CNIPOL_DIR/bin
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CNIPOL_DIR/lib
 fi
 
 #export CNIPOL_DIR=~/cnipol/cnipol/trunk
-export POLARIM_DIR=/usr/local/polarim
+export POLARIM_DIR=/data1/run11
 
 #export CNILIB=/home/dsmirnov/cnipol_install
 #export CNIPOL_LIB_DIR=$CNIPOL_DIR/lib
