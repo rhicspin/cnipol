821f178a46d21dc314dc43198a74df68894aa4f3
Dmitri Smirnov <dsmirnov@bnl.gov>
2011-03-08 16:22:10 +0000

    Downstream pol daemon is on yellowpc now which is not in the tunnel
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1193 cd24ecce-3d09-47a0-95ec-c030155e877a

--- online/mcr/bin/start.sh
+++ online/mcr/bin/start.sh
@@ -9,13 +9,19 @@ HOSTNAME=`hostname -s`
 if [ $HOSTNAME = "bluepc" ]; then
 	#echo $HOSTNAME
 	./rpoldaemon.sh -U >>/dev/null &
-	exit $status 
+	exit $status
 else
 	if [ $HOSTNAME = "insideip12" ]; then
 		#echo $HOSTNAME
 		./rpoldaemon.sh -D >>/dev/null &
-		exit $status 
+		exit $status
 	else
-		echo "Host not recognized. Start script on either bluepc or insideip12"
+		if [ $HOSTNAME = "yellowpc" ]; then
+			#echo $HOSTNAME
+			./rpoldaemon.sh -D >>/dev/null &
+			exit $status
+		else
+			echo "Host not recognized. Start script on either bluepc, insideip12, or yellowpc"
+		fi
 	fi
 fi
