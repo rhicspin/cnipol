57fdb4cf759a928c9f207da5cd5d0f679564f77d
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-04-12 21:24:32 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1264 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ ana/CnipolTimeMHists.cxx
@@ -0,0 +1,112 @@
+/*****************************************************************************
+ *                                                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#include "CnipolTimeMHists.h"
+
+#include "RunInfo.h"
+
+
+ClassImp(CnipolTimeMHists)
+
+using namespace std;
+
+/** Default constructor. */
+CnipolTimeMHists::CnipolTimeMHists() : DrawObjContainer()
+{
+   BookHists();
+}
+
+
+CnipolTimeMHists::CnipolTimeMHists(TDirectory *dir) : DrawObjContainer(dir)
+{
+   BookHists();
+}
+
+
+/** Default destructor. */
+CnipolTimeMHists::~CnipolTimeMHists()
+{
+}
+
+
+/** */
+void CnipolTimeMHists::BookHists(string sid)
+{ //{{{
+   char hName[256];
+   char htitle[256];
+
+   fDir->cd();
+
+   sprintf(hName, "bunch_dist_raw");
+   sprintf(htitle, "%.3f: Raw Counts per Bunch ", gRunInfo.RUNID);
+   o[hName] = new TH1F(hName, htitle, NBUNCH, -0.5, NBUNCH-0.5);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("Bunch Id");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("Events");
+   ((TH2F*) o[hName])->SetFillColor(kGray);
+
+   sprintf(hName, "strip_dist_raw");
+   sprintf(htitle, "%.3f: Raw Counts per Strip ", gRunInfo.RUNID);
+   o[hName] = new TH1F(hName, htitle, NSTRIP, 1, NSTRIP);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("Strip Id");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("Events");
+   ((TH2F*) o[hName])->SetFillColor(kGray);
+
+   sprintf(hName, "tdc_raw");
+   sprintf(htitle, "%.3f: Raw TDC (All Strips)", gRunInfo.RUNID);
+   o[hName] = new TH1F(hName, htitle, 100, 0, 100);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("TDC");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("Events");
+   ((TH2F*) o[hName])->SetFillColor(kGray);
+
+   sprintf(hName, "adc_raw");
+   sprintf(htitle, "%.3f: Raw ADC (All Strips)", gRunInfo.RUNID);
+   o[hName] = new TH1F(hName, htitle, 255, 0, 255);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("ADC");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("Events");
+   ((TH2F*) o[hName])->SetFillColor(kGray);
+
+   sprintf(hName, "tdc_vs_adc_raw");
+   sprintf(htitle, "%.3f: Raw TDC vs ADC (All Strips)", gRunInfo.RUNID);
+   o[hName] = new TH2F(hName, htitle, 255, 0, 255, 100, 0, 100);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("ADC");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("TDC");
+   ((TH2F*) o[hName])->SetFillColor(kGray);
+
+   sprintf(hName, "tdc_vs_adc_false_bunch_raw");
+   sprintf(htitle, "%.3f: Raw TDC vs ADC (All Strips) false bunch", gRunInfo.RUNID);
+   o[hName] = new TH2F(hName, htitle, 255, 0, 255, 100, 0, 100);
+   ((TH2F*) o[hName])->GetXaxis()->SetTitle("ADC");
+   ((TH2F*) o[hName])->GetYaxis()->SetTitle("TDC");
+   ((TH2F*) o[hName])->SetFillColor(kRed);
+} //}}}
+
+
+/** */
+void CnipolTimeMHists::Print(const Option_t* opt) const
+{ //{{{
+   opt = "";
+
+   //printf("CnipolTimeMHists:\n");
+   DrawObjContainer::Print();
+} //}}}
+
+
+/** */
+void CnipolTimeMHists::Fill(ChannelEvent *ch, string sid)
+{ //{{{
+   // Fill events with no cuts applied
+   if (sid == "") {
+
+      ((TH1*) o["bunch_dist_raw"])->Fill(ch->GetChannelId());
+      ((TH1*) o["strip_dist_raw"])->Fill(ch->GetBunchId());
+      ((TH1*) o["tdc_raw"])       ->Fill(ch->GetTdc());
+      ((TH1*) o["adc_raw"])       ->Fill(ch->GetAmpltd());
+      ((TH1*) o["tdc_vs_adc_raw"])->Fill(ch->GetAmpltd(), ch->GetTdc());
+
+   } else if (sid == "cut_false_bunch") {
+
+      ((TH1*) o["tdc_vs_adc_false_bunch_raw"])->Fill(ch->GetAmpltd(), ch->GetTdc());
+   }
+} //}}}
