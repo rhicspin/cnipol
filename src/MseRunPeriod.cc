cc72bc0774cc4caf43f283e75c00ecdf6a24c4c9
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-04-19 14:46:45 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1279 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ src/MseRunPeriod.cc
@@ -0,0 +1,40 @@
+
+#define EXPAND_MY_SSQLS_STATICS
+
+#include "MseRunPeriod.h"
+
+using namespace std;
+
+MseRunPeriodX::MseRunPeriodX(const mysqlpp::sql_datetime &p1) : MseRunPeriod(p1)
+{
+   Init();
+}
+
+void MseRunPeriodX::Init()
+{
+   start_time           = mysqlpp::DateTime("0000-00-00 00:00:00");
+   polarimeter_id       = -1;
+   alpha_calib_run_name = "";
+   dl_calib_run_name    = "";
+   disabled_channels    = "";
+   disabled_bunches     = "";
+}
+
+
+/** */
+void MseRunPeriodX::Print(const Option_t* opt) const
+{
+   PrintAsPhp();
+}
+
+
+/** */
+void MseRunPeriodX::PrintAsPhp(FILE *f) const
+{ //{{{
+   fprintf(f, "$rc['start_time']                     = %d;\n", (int) time_t(start_time)        );
+   fprintf(f, "$rc['polarimeter_id']                 = %d;\n", (short) polarimeter_id    );
+   fprintf(f, "$rc['alpha_calib_run_name']           = \"%s\";\n", string(alpha_calib_run_name).c_str());
+   fprintf(f, "$rc['dl_calib_run_name']              = \"%s\";\n", dl_calib_run_name.data() );
+   fprintf(f, "$rc['disabled_channels']              = \"%s\";\n", disabled_channels.c_str() );
+   fprintf(f, "$rc['disabled_bunches']               = \"%s\";\n", disabled_bunches.c_str()  );
+} //}}}
