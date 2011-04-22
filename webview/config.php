2c261046183622105a582280f2973ffc66765586
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-04-22 22:00:15 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1294 cd24ecce-3d09-47a0-95ec-c030155e877a

--- webview/config.php
+++ webview/config.php
@@ -6,7 +6,8 @@ $POLARIMETER_ID = array(0 => "B1U", 1 => "Y1D", 2 => "B2D", 3 => "Y2U");
 $RHIC_BEAM      = array("1" => "Yellow", "2" => "Blue");
 $RHIC_STREAM    = array("1" => "Upstream", "2" => "Downstream");
 $MEASTYPE       = array(0x00 => "undef", 0x01 => "alpha", 0x02 => "sweep", 0x04 => "fixed", 0x08 => "ramp", 0x10 => "emit");
-$TARGET_ORIENT  = array("H" => "Horizontal", "V" => "Vertical");
-$TARGET_ID      = range(1, 6);
+$BEAM_ENERGY    = array("24" => "24", "100" => "100", "250" => "250");
+$TARGET_ORIENT  = array("H" => "H", "V" => "V");
+$TARGET_ID      = array_combine(range(1, 6), range(1, 6));
 
 ?>
