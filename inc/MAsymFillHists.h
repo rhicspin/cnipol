f41bc6603753def0d2a04025aaa250cda90d4463
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-05-12 23:34:44 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1325 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ inc/MAsymFillHists.h
@@ -0,0 +1,49 @@
+/*****************************************************************************
+ *                                                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+
+#ifndef MAsymFillHists_h
+#define MAsymFillHists_h
+
+#include "TDirectory.h"
+
+#include "AsymGlobals.h"
+#include "ChannelEvent.h"
+#include "DrawObjContainer.h"
+#include "RunConfig.h"
+
+
+/**
+ *
+ */
+class MAsymFillHists : public DrawObjContainer
+{
+public:
+
+
+private:
+
+   std::map<Double_t, UInt_t> fHTargetVsRun;
+   std::map<Double_t, UInt_t> fVTargetVsRun;
+
+public:
+
+   MAsymFillHists();
+   MAsymFillHists(TDirectory *dir);
+   ~MAsymFillHists();
+
+   void BookHists(std::string sid="");
+   void BookHistsPolarimeter(EPolarimeterId polId, EBeamEnergy beamE);
+   //void BookHistsEnergy(EBeamEnergy beamE);
+   void Fill(ChannelEvent *ch, std::string cutid="");
+   //void Fill(Int_t n, Double_t* hData);
+   void Fill(EventConfig &rc);
+   void PostFill();
+   void Print(const Option_t* opt="") const;
+
+   ClassDef(MAsymFillHists, 1)
+};
+
+#endif
