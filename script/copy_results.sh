a1053ec378a4a295f37585f3b74cedfc834663f3
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2011-05-25 16:31:21 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1350 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/copy_results.sh
+++ script/copy_results.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
 SERVICE="rsync"
-RESULT=`ps -a | sed -n /${SERVICE}/p`
+CHECKINGPERIOD=3600                  # in sec
 
-if [ "${RESULT:-null}" = null ]; then
-   echo "rsync not running. Executing"
-	#echo "rsync -a /eicdata/eic0005/run11/root/* bluepc:/usr/local/polarim/root/"
-	#echo "rsync -a /eicdata/eic0005/run09/root/* bluepc:/usr/local/polarim/root/"
-	echo "rsync -a eic0005:/eicdata/eic0005/run09/root/* /usr/local/polarim/root/"
-	echo "rsync -a eic0005:/eicdata/eic0005/run11/root/* /usr/local/polarim/root/"
-else
-   echo "running"
-fi
+while [ 1=1 ];
+do
+    RESULT=`ps -a | sed -n /${SERVICE}/p`
+    if [ "${RESULT:-null}" = null ]; then
+      echo "rsync not running. Executing"
+    	#echo "rsync -a /eicdata/eic0005/run11/root/* bluepc:/usr/local/polarim/root/"
+    	#echo "rsync -a /eicdata/eic0005/run09/root/* bluepc:/usr/local/polarim/root/"
+    	nohup rsync -av eic0005:/eicdata/eic0005/run09/root/* /usr/local/polarim/root/ &
+    	nohup rsync -av eic0005:/eicdata/eic0005/run11/root/* /usr/local/polarim/root/ &
+    else
+       echo "running"
+    fi
+    sleep $CHECKINGPERIOD
 
+done
+
+#MAILSTOP=0                         # switcher for mail sending
+#CHECKINGPERIOD=20                  # in sec
+#MYEMAIL="me@myemail.com"           # where to send info
+#
+#while [ 1=1 ];
+#do
+#    if [ ! "$(pidof httpd)" ]
+#    then
+#    else
+#    fi
+#    sleep $CHECKINGPERIOD
