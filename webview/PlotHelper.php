46d07f237b50adfcdcbc094ea265d72e128402a0
Dmitri Smirnov <dsmirnov@bnl.gov>
2011-07-06 22:17:19 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1373 cd24ecce-3d09-47a0-95ec-c030155e877a

--- webview/PlotHelper.php
+++ webview/PlotHelper.php
@@ -10,13 +10,19 @@ class PlotHelper {
       $this->ps = $ps;
    }
 
-   function img($pName, $w=300) {
+   function img($pName, $w=300, $href=null)
+   {
 	   $pi = pathinfo($pName);
+
 	   $imgName = "{$this->dir}/{$pi['dirname']}/c_{$pi['basename']}.png";
-      $html = "<a href=$imgName><img width=$w src=$imgName></a>";
+
+      if (empty($href)) {
+         $href = $imgName;
+      }
+
+      $html = "<a href=\"$href\"><img width=$w src=\"$imgName\"></a>";
 		return $html;
    }
-
 }
 
 ?>
