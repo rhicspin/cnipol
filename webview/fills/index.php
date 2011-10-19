e01c599686c0d2eb4b02256385091542ea8195f6
Dmitri Smirnov <dsmirnov@bnl.gov>
2011-10-19 20:56:10 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1419 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ webview/fills/index.php
@@ -0,0 +1,12 @@
+<?php
+
+include("../utils.php"); 
+include("../config.php");
+
+//date_default_timezone_set('GMT');
+setlocale(LC_TIME, 'en');
+putenv("TZ=America/New_York");
+
+include("fills_index.html");
+
+?>
