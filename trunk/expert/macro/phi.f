470c6cfa20056935830c3680b983855960f6b07e
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2012-02-02 19:46:07 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/tags/v1.8.1@1550 cd24ecce-3d09-47a0-95ec-c030155e877a

--- trunk/expert/macro/phi.f
+++
@@ -1,7 +0,0 @@
-
-	function phi(x)
-	real x
-	common/pawpar/par(2)
-	phi = par(1)/100*sin(-x+par(2)/180*3.141592)
-*	phi = par(1)*sin(-x+par(2))
-	end
--- trunk/webview/rundb/config_runinfo_plots.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-$plots_set1['tdc_raw'] = "";
-$plots_set1['tdc_vs_adc_false_bunch_raw'] =  "";
-$plots_set1['tdc_vs_adc_raw'] =  "";
-$plots_set1['wall_current_monitor'] =  "";
-
-?>
