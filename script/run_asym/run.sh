3fab56d8c48be5da11b9cceb7b0a23d44dba407d
Dmitri Smirnov <dsmirnov@bnl.gov>
2012-04-09 22:57:48 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1719 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym/run.sh
+++ script/run_asym/run.sh
@@ -1,4 +1,4 @@
-source ~/run/bgx_asym.sh
+source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
 
 #bgx_limit 3 sleep 10
 
@@ -12,8 +12,9 @@ do
    #echo "bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy";
    #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
    #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db --copy
-   bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
+   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
    #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama
+   bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
 done
 
 # Wait until all others are finished.
