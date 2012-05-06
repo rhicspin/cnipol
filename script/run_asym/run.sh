ae75a834af6b5396aa62a8329baf81071e236ea9
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2012-05-06 15:21:46 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1731 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym/run.sh
+++ script/run_asym/run.sh
@@ -2,20 +2,22 @@ source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
 
 #bgx_limit 3 sleep 10
 
+export SSH_AUTH_SOCK=/tmp/ssh-qhSiH31025/agent.31025
+export SSH_AGENT_PID=31026
+
 FILE_RUNS=$1
 
 exec < $FILE_RUNS
 
 while read run_name
 do
-   #echo "bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db";
-   #echo "bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy";
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db --copy
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama
-   bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db --copy
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama
+   #bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
+   bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema
 done
 
-# Wait until all others are finished.
+# Wait until all processes are finished
 bgx_wait
--- script/run_asym/run_by_pattern.sh
+++ script/run_asym/run_by_pattern.sh
@@ -28,9 +28,9 @@ echo
 for file_name in ${RUN_LIST[@]}
 do
    run_name=${file_name%.data} 
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --pmt --copy
-   #bgx_limit 16 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --disable-det=101101 --sfx=hama --raw
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --pmt --copy
+   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --disable-det=101101 --sfx=hama --raw
    bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
    #bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema 
 done
