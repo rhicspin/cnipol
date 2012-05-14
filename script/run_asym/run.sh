7cf1bb408570ddd8e50a139da831e8055615886d
Dmitri Smirnov <dsmirnov@bnl.gov>
2012-05-14 19:53:03 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1736 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym/run.sh
+++ script/run_asym/run.sh
@@ -1,22 +1,22 @@
-source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
+#!/bin/bash
 
-#bgx_limit 3 sleep 10
+source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
 
 export SSH_AUTH_SOCK=/tmp/ssh-qhSiH31025/agent.31025
 export SSH_AGENT_PID=31026
 
-FILE_RUNS=$1
+FILE_MEASLIST=$1
 
-exec < $FILE_RUNS
+exec < $FILE_MEASLIST
 
-while read run_name
+while read meas_name
 do
-   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy
-   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db --copy
-   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
-   #bgx_limit 10 ./bin/asym -r $run_name --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama
-   #bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
-   bgx_limit 10 $CNIPOL_DIR/bin/asym -r $run_name --calib --profile --use-db --raw-ext --asym --pmt --kinema
+   #bgx_limit 10 ./bin/asym -r $meas_name --calib --profile --use-db -g --copy
+   #bgx_limit 10 ./bin/asym -r $meas_name --calib --profile --use-db --copy
+   #bgx_limit 10 ./bin/asym -r $meas_name --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy 
+   #bgx_limit 10 ./bin/asym -r $meas_name --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama
+   #bgx_limit 10 $CNIPOL_DIR/bin/asym -r $meas_name --calib --profile --use-db --raw-ext --asym --pmt --kinema -g
+   bgx_limit 10 $CNIPOL_DIR/bin/asym -r $meas_name --calib --profile --use-db --raw-ext --asym --pmt --kinema
 done
 
 # Wait until all processes are finished
