b8d426bcf691ddee9b7731753e42b53dc1b1b10c
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2012-06-22 21:50:19 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1776 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/masym.cc
+++ src/masym.cc
@@ -153,6 +153,7 @@ int main(int argc, char *argv[])
       if ( polarization < 10 || polarization > 99 || polarizationErr > 30 ||
           gRunConfig.fBeamEnergies.find(beamEnergy) == gRunConfig.fBeamEnergies.end() ||
           gMM->fMeasInfo->fMeasType != kMEASTYPE_SWEEP ||
+          (TMath::Abs(profileRatio) > 5.000) ||                           // exclude very large values
           (TMath::Abs(profileRatio) > 1.000 && profileRatioErr < 0.05) || // exclude large values with small errors
           (TMath::Abs(profileRatio) > 1.000 && profileRatioErr > 0.50) || // exclude large values with large errors
           (profileRatioErr/TMath::Abs(profileRatio) > 2.000) ||
