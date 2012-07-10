dc794dde0785aeba13d408bb822b5286116a6c31
Dmitri Smirnov <dsmirnov@bnl.gov>
2012-07-10 18:54:17 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1787 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/masym.cc
+++ src/masym.cc
@@ -156,7 +156,7 @@ int main(int argc, char *argv[])
           (TMath::Abs(profileRatio) > 5.000) ||                           // exclude very large values
           (TMath::Abs(profileRatio) > 1.000 && profileRatioErr < 0.05) || // exclude large values with small errors
           (TMath::Abs(profileRatio) > 1.000 && profileRatioErr > 0.50) || // exclude large values with large errors
-          (profileRatioErr/TMath::Abs(profileRatio) > 2.000) ||
+          (profileRatioErr/TMath::Abs(profileRatio) > 2.000 && TMath::Abs(profileRatio) > 1.0) ||
           (profileRatioErr < 0.01)
           //(TMath::Abs(profileRatio) < 0.001 && profileRatioErr < 0.01)
          )
