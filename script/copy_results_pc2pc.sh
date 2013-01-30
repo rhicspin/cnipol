9de55727396a2f571dfaceed73a71257a94ab06c
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2013-01-30 23:56:33 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1815 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/copy_results_pc2pc.sh
+++ script/copy_results_pc2pc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# To run this script do
+#
+# >eval `ssh-agent -s`
+# >ssh-add
+#
+# Then modify the SSH_... variables in this script accordingly
+#
+# >nohup ./copy_results_pc2pc.sh run12
+#
+
 RUN_PERIOD=$1
 
 LOG="/eic/u/dsmirnov/eic0005/copy_results_pc2pc_${RUN_PERIOD}.log"
@@ -7,9 +17,16 @@ CHECKINGPERIOD=3600 # in sec
 
 #eval `ssh-agent -s` >> ${LOG} 2>&1
 #export 
-export SSH_AUTH_SOCK=/tmp/ssh-lxuXX32554/agent.32554
-export SSH_AGENT_PID=32555
 
+#eic0008
+#export SSH_AUTH_SOCK=/tmp/ssh-Slbbvtp782/agent.782
+#export SSH_AGENT_PID=783
+#eic0009
+export SSH_AUTH_SOCK=/tmp/ssh-QnbKyj9381/agent.9381
+export SSH_AGENT_PID=9382
+#eic0010
+#export SSH_AUTH_SOCK=/tmp/ssh-TPtHZa4668/agent.4668
+#export SSH_AGENT_PID=4669
 
 while [ 1 ];
 do
