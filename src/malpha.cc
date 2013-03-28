c29b29d12c8e8c6f3fcdd4155e4cba8d4ef02542
Dmitry Kalinkin <dkalinkin@bnl.gov>
2013-03-28 19:02:33 +0000

    malpha: remove some code
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@1997 cd24ecce-3d09-47a0-95ec-c030155e877a

--- src/malpha.cc
+++ src/malpha.cc
@@ -18,24 +18,6 @@ using namespace std;
 
 
 /** */
-void FillFromHist(TH1F *h, double startTime, map< double, vector<double> > &result, map< double, vector<double> > &result_err)
-{
-   Int_t xfirst  =  h->GetXaxis()->GetFirst();
-   Int_t xlast   =  h->GetXaxis()->GetLast();
-
-   assert(h->GetNbinsX() == N_SILICON_CHANNELS);
-   if (result.count(startTime) == 0) {
-      result[startTime].resize(N_SILICON_CHANNELS);
-      result_err[startTime].resize(N_SILICON_CHANNELS);
-      for (int i = xfirst; i <= xlast; i++) {
-         result[startTime][i - xfirst] = h->GetBinContent(i);
-         result_err[startTime][i - xfirst] = h->GetBinError(i);
-      }
-   }
-}
-
-
-/** */
 TH1F* PlotMean(const char *name, map< double, double > &result, map< double, double > &result_err, double min_startTime, double max_startTime)
 {
    double min_value = FLT_MAX, max_value = -FLT_MAX;
@@ -96,12 +78,6 @@ int main(int argc, char *argv[])
 
    Info("malpha", "Starting first pass...");
 
-   map< Short_t, map< double, vector<double> > > result_am;
-   map< Short_t, map< double, vector<double> > > result_amgd;
-   map< Short_t, map< double, vector<double> > > result_fit0;
-   map< Short_t, map< double, vector<double> > > result_am_err;
-   map< Short_t, map< double, vector<double> > > result_amgd_err;
-   map< Short_t, map< double, vector<double> > > result_fit0_err;
    map< Short_t, map< double, double > > result_am_amgd_mean;
    map< Short_t, map< double, double > > result_am_amgd_mean_err;
    map< Short_t, map< double, double > > result_fit0mean;
@@ -166,14 +142,8 @@ int main(int argc, char *argv[])
          min_startTime = startTime;
       }
 
-      TH1F  *hAmAmpCoef = (TH1F*) f->FindObjectAny("hAmAmpCoef");
-      TH1F  *hAmGdAmpCoef = (TH1F*) f->FindObjectAny("hAmGdAmpCoef");
       TH1F  *hAmGdAmpCoef_over_AmAmpCoef = (TH1F*) f->FindObjectAny("hAmGdAmpCoef_over_AmAmpCoef");
       TH1F  *hAmGdFit0Coef = (TH1F*) f->FindObjectAny("hAmGdFit0Coef");
-
-      FillFromHist(hAmAmpCoef, startTime, result_am[polId], result_am_err[polId]);
-      FillFromHist(hAmGdAmpCoef, startTime, result_amgd[polId], result_amgd_err[polId]);
-      FillFromHist(hAmGdFit0Coef, startTime, result_fit0[polId], result_fit0_err[polId]);
 
       {
          TFitResultPtr fitres = hAmGdFit0Coef->Fit("pol0", "S"); // S: return fitres
@@ -201,32 +171,6 @@ int main(int argc, char *argv[])
 
       TDirectory *fDir = f1.mkdir(polIdName.c_str());
       fDir->cd();
-
-      for (int channel_id = 0; channel_id < N_SILICON_CHANNELS; channel_id++) {
-         TString obj_name("AmGain_over_AmGdGain_");
-         obj_name += channel_id;
-         const char *obj_name_cstr = obj_name.Data();
-
-         TH1F *h = new TH1F(obj_name_cstr, obj_name_cstr, 10 * result_am.size(), -86400, max_startTime - min_startTime + 86400);
-         for (map< double, vector<double> >::iterator it = result_am[polId].begin(); it != result_am[polId].end(); it++) {
-            double startTime = it->first;
-            double value = result_am[polId][startTime][channel_id] / result_amgd[polId][startTime][channel_id];
-
-            h->SetBinContent(
-               h->FindBin(startTime - min_startTime),
-               value
-            );
-            h->SetBinError(
-               h->FindBin(startTime - min_startTime),
-               value *
-               (result_am_err[polId][startTime][channel_id] / result_am[polId][startTime][channel_id]
-                + result_amgd_err[polId][startTime][channel_id] / result_amgd[polId][startTime][channel_id])
-            );
-         }
-         h->GetXaxis()->SetTimeDisplay(1);
-         h->GetXaxis()->SetTimeFormat("%d.%m.%Y");
-         h->GetXaxis()->SetTimeOffset(min_startTime);
-      }
 
       PlotMean("hAmGdFit0Coef", result_fit0mean[polId], result_fit0mean_err[polId], min_startTime, max_startTime);
       PlotMean("hAmGdAmpCoef_over_AmAmpCoef", result_am_amgd_mean[polId], result_am_amgd_mean_err[polId], min_startTime, max_startTime);
