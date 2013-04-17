4e983e5bdbb4b42b32554619b7a8678132284694
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2013-04-17 22:22:57 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@2043 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym/run.sh
+++ script/run_asym/run.sh
@@ -12,7 +12,7 @@ do
    #bgx_limit 10 ./bin/asym --calib --profile --use-db --copy -r $meas_name
    #bgx_limit 10 ./bin/asym --calib --profile --use-db --pmt --raw-ext --asym --kinema -g --copy  -r $meas_name
    #bgx_limit 10 ./bin/asym --calib --profile --use-db -g --copy --raw --asym --disable-det=101101 --sfx=hama -r $meas_name
-   bgx_limit 10 $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -g -r $meas_name
+   bgx_limit 10 $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -g --copy -r $meas_name
    #bgx_limit 10 $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -r $meas_name
 done
 
