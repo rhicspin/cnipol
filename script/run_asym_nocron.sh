5b1f2ab1577864539831e0914673d0ce83a26725
Dmitri Smirnov <dsmirnov@bnl.gov>
2013-04-17 22:22:22 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@2042 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym_nocron.sh
+++ script/run_asym_nocron.sh
@@ -10,25 +10,25 @@
 # >nohup ./copy_results_pc2pc.sh run12
 #
 
-source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
+#source $CNIPOL_DIR/script/run_asym/bgx_asym.sh
 
-#LOG="/usr/local/polarim/log/run_asym_nocron.log"
-LOG="/eicdata/eic0005/run13/run_asym_nocron.log"
+LOG="/usr/local/polarim/log/run_asym_nocron.log"
+#LOG="/eicdata/eic0005/run13/run_asym_nocron.log"
 CHECKINGPERIOD=60 # in sec
 
 # pc2pc-phy
-#export SSH_AUTH_SOCK=/tmp/ssh-CnylBq9610/agent.9610
-#export SSH_AGENT_PID=9611
+export SSH_AUTH_SOCK=/tmp/ssh-CnylBq9610/agent.9610
+export SSH_AGENT_PID=9611
 # eic0005
-export SSH_AUTH_SOCK=/tmp/ssh-lKLHL25172/agent.25172
-export SSH_AGENT_PID=25173
+#export SSH_AUTH_SOCK=/tmp/ssh-lKLHL25172/agent.25172
+#export SSH_AGENT_PID=25173
 
-#source /usr/local/cnipol/setup.sh >> ${LOG} 2>&1
-source /eicdata/eic0005/cnipol/setup13.sh >> ${LOG} 2>&1
+source /usr/local/cnipol/setup.sh >> ${LOG} 2>&1
+#source /eicdata/eic0005/cnipol/setup13.sh >> ${LOG} 2>&1
 
 # overwrite the data location
-#export CNIPOL_DATA_DIR=/usr/local/polarim/data_buffer
-export CNIPOL_DATA_DIR=/eicdata/eic0005/run13/data_buffer
+export CNIPOL_DATA_DIR=/usr/local/polarim/data_buffer
+#export CNIPOL_DATA_DIR=/eicdata/eic0005/run13/data_buffer
 echo "overwrite data location" >> ${LOG}
 echo "\$CNIPOL_DATA_DIR=$CNIPOL_DATA_DIR" >> ${LOG}
 
@@ -56,12 +56,12 @@ do
    do
       #echo processed item : ${run_name%.data} and removed
       echo $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -g -r ${run_name%.data} >> ${LOG}
-      #$CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -g -r ${run_name%.data} >> ${LOG} 2>&1
-      bgx_limit 10 $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -r ${run_name%.data} >> ${LOG}
+      $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -g -r ${run_name%.data} >> ${LOG} 2>&1
+      #bgx_limit 10 $CNIPOL_DIR/bin/asym --calib --profile --use-db --raw-ext --asym --pmt --kinema -r ${run_name%.data} >> ${LOG}
    done
 
    # Wait until all processes are finished
-   bgx_wait
+   #bgx_wait
 
    for run_name in "${array[@]}"
    do
