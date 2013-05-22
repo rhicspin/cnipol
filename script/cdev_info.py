da6b72936c9033e659424c406fe5be3737560345
Dmitri Smirnov <dsmirnov@bnl.gov>
2013-05-22 15:23:33 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@2062 cd24ecce-3d09-47a0-95ec-c030155e877a

---
+++ script/cdev_info.py
@@ -0,0 +1,33 @@
+import sys
+import string
+import os
+
+
+
+def main():
+
+   #fileAllMeas = open("/eicdata/eic0005/runXX/lists/run13_phys")
+   fileAllMeas = open("/eicdata/eic0005/runXX/lists/run13_new")
+   allMeass    = fileAllMeas.readlines()
+   fills = []
+
+   for meas in allMeass:
+      meas = meas.strip()
+      fillId, measId = meas.split('.')
+      print "meas: %s" % (meas),
+
+      if (fillId not in fills):
+         fills.append(fillId)
+         print "insert in fills: %s\n" % (fillId),
+      else:
+         print "already in fills %s\n" % (fillId),
+
+   for fillId in fills:
+      cmd = "ssh -Y acnlina \"~/cnipol_trunk/script/get_fill_info.sh %s\" > /eicdata/eic0005/runXX/cdev_info/cdev_%s" % (fillId, fillId)
+      print "%s\n" % (cmd),
+      os.system(cmd)
+
+
+
+if __name__ == '__main__':
+    main()
