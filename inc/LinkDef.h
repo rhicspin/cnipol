9a5831c4ff5f8739b7837bb7ef2179dc61fa84ce
Dmitry Kalinkin <dkalinkin@bnl.gov>
2013-07-24 18:59:01 +0000

    remove ghost classes: MAlphaRoot, HistManager, HistContainer
    git-svn-id: https://svn.bnl.gov/cnipol/trunk@2144 cd24ecce-3d09-47a0-95ec-c030155e877a

--- inc/LinkDef.h
+++ inc/LinkDef.h
@@ -41,10 +41,7 @@
 #pragma link C++ class CnipolScalerHists+;
 #pragma link C++ class CnipolSpinStudyHists+;
 #pragma link C++ class CnipolTargetHists+;
-#pragma link C++ class HistContainer+;
-#pragma link C++ class HistManager+;
 #pragma link C++ class MAlphaAnaInfo+;
-#pragma link C++ class MAlphaRoot+;
 #pragma link C++ class MAsymAnaInfo+;
 #pragma link C++ class MAsymFillHists+;
 #pragma link C++ class MAsymRateHists+;
