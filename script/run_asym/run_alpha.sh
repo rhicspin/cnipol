345b7e4b7c945a82591b6cebd991c8b06f4d9877
dsmirnov@BNL.GOV <dsmirnov@BNL.GOV@cd24ecce-3d09-47a0-95ec-c030155e877a>
2013-07-29 15:45:02 +0000

    git-svn-id: https://svn.bnl.gov/cnipol/trunk@2150 cd24ecce-3d09-47a0-95ec-c030155e877a

--- script/run_asym/run_alpha.sh
+++ script/run_asym/run_alpha.sh
@@ -17,8 +17,7 @@ do
    if [ -z "${arr[1]}" ]; then
       #echo "polid NOT defined"
       #bgx_limit 10 $CNIPOL_DIR/bin/asym --alpha --use-db -g --copy -r $meas_name --no-update-db
-      #bgx_limit 10 $CNIPOL_DIR/bin/asym --alpha --use-db -g --copy -r $meas_name
-      bgx_limit 10 $CNIPOL_DIR/bin/asym --alpha --use-db -r $meas_name
+      bgx_limit 10 $CNIPOL_DIR/bin/asym --alpha --use-db -g --copy -r $meas_name
    else
       #echo "polid defined" ${arr[1]}
       bgx_limit 10 $CNIPOL_DIR/bin/asym --alpha --use-db -g --copy --pol-id=${arr[1]} -r ${arr[0]} --no-update-db
