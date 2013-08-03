eacafe6cbbdb68ecafdd7feaff8572df783fd8a4
Dmitry Kalinkin <dkalinkin@bnl.gov>
2013-08-02 22:45:47 -0400

    get_revision_id improved: use git tags, only update revision-id file on revision change, add github links to rundb online page

--- get_revision_id.sh
+++ get_revision_id.sh
@@ -1,50 +1,39 @@
 #!/bin/bash
 
-echo -n "const char *ASYM_VERSION = \""
+function print_version_definition()
+{
+	echo -n "const char *ASYM_VERSION = \""
 
-# check if git is in PATH
-which git 1> /dev/null 2> /dev/null
-if [ $? != 1 ]; then
-	git diff --quiet
-	UNSTAGED=$?
-	GIT_FOUND=1
-else
-	UNSTAGED=0
-	GIT_FOUND=0
-fi
-
-# if this is not a git repo or git is not found fallback to svn
-if [ $UNSTAGED = 129 ] || [ $GIT_FOUND = 0 ]; then
-	SVNVERSION=`svnversion $1`
-	if [ $SVNVERSION = "exported" ]; then
-		echo "not in repo\""
-		exit 0
+	# check if git is in PATH
+	which git 1> /dev/null 2> /dev/null
+	if [ $? != 1 ]; then
+		git diff --quiet
+		UNSTAGED=$?
+		GIT_FOUND=1
 	else
-		echo "${SVNVERSION}\";"
-		exit 0
+		UNSTAGED=0
+		GIT_FOUND=0
 	fi
-fi
 
-# check if there were staged changes
-git diff --cached --quiet
-STAGED=$?
+	# check if there were staged changes
+	git diff --cached --quiet
+	STAGED=$?
 
-if [ $STAGED = 1 ] || [ $UNSTAGED = 1 ]; then
-	FLAGS="M"
-else
-	# check if last git commit is pushed to svn
-	SVN_ID_LINE=`git log -1 --pretty=%B | grep "^git-svn-id: "`
-	if [ $? != 0 ]; then
+	git describe --abbrev=0 --tags --exact 2> /dev/null
+	UNSTABLE=$?
+
+	if [ $STAGED = 1 ] || [ $UNSTAGED = 1 ] || [ $UNSTABLE = 128 ]; then
 		FLAGS="M"
 	fi
-fi
 
-# Take last svn revision id.
-SVN_ID_LINE=`git log --pretty=%B | grep "^git-svn-id: "  | head -n 1`
-if [ $? = 0 ]; then
-	SVNVERSION=`echo $SVN_ID_LINE | cut -d" " -f2 | cut -d"@" -f2`
-	echo "${SVNVERSION}${FLAGS}\";"
-	exit 0
-fi
+	TAG=`git describe --abbrev=0 --tags 2> /dev/null`
+	echo -n "${TAG}${FLAGS};"
+	git rev-parse HEAD | xargs echo -n
+	echo "\";"
+}
 
-echo "unknown\";"
+DEFINITION_STR=`print_version_definition`
+FILE_CONTENTS=`cat $2 2> /dev/null`
+if [ "$DEFINITION_STR" != "$FILE_CONTENTS" ]; then
+	echo $DEFINITION_STR > $2
+fi
