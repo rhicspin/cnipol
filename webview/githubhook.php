2d3af83434e3d22fe7929f8262a51fa74587dec9
Dmitry Kalinkin <dkalinkin@bnl.gov>
2013-08-05 18:29:16 -0400

    githubhook: stash local changes that may prevent update

--- webview/githubhook.php
+++ webview/githubhook.php
@@ -9,7 +9,7 @@ if ((($ip & $mask_27) === ip2long('204.232.175.64')) || (($ip & $mask_22) === ip
 {
 	$payload = $_POST['payload'];
 	if (!$payload) die('no');
-	system('git fetch 1>/dev/null 2>/dev/null; git checkout origin/master; cd /home/cnipol/cnipol_web_production/doc; doxygen 1>/dev/null 2>/dev/null');
+	system('git fetch 1>/dev/null 2>/dev/null; git stash 1>/dev/null; git checkout origin/master; cd /home/cnipol/cnipol_web_production/doc; doxygen 1>/dev/null 2>/dev/null');
 	echo 'ok';
 }
 else
