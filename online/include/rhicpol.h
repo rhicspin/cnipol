4b5827c7e6b031aaa91a2fd6a432d8044cc07dcf
Dmitry Kalinkin <dkalinkin@bnl.gov>
2013-08-18 19:56:33 -0400

    rhicpol.h: fixed size types, 64bit-friendly structures

--- online/include/rhicpol.h
+++ online/include/rhicpol.h
@@ -56,31 +56,33 @@
 #define CFG_INIT        0
 #define CFG_UPDATE      1
 
+#include <stdint.h>
+
 #include "static_assert.h"
 
 //extern char polCDEVName[4][20];// = {"polarimeter.blu1", "polarimeter.blu2", "polarimeter.yel1", "polarimeter.yel2"};
 
 typedef struct {
     double runIdS;
-    long startTimeS;
-    long stopTimeS;
+    int32_t startTimeS;
+    int32_t stopTimeS;
     char daqVersionS[80];
     char cutIdS[80];
     char targetIdS[80];
-    long encoderPositionS[2];
-    long statusS;
+    int32_t encoderPositionS[2];
+    int32_t statusS;
     char statusStringS[80];
     //
-    long totalCountsS;
-    long upCountsS;
-    long downCountsS;
-    long unpolCountsS;
-    long countsUpLeftS[360];
-    long countsLeftS[360];
-    long countsDownLeftS[360];
-    long countsDownRightS[360];
-    long countsRightS[360];
-    long countsUpRightS[360];
+    int32_t totalCountsS;
+    int32_t upCountsS;
+    int32_t downCountsS;
+    int32_t unpolCountsS;
+    int32_t countsUpLeftS[360];
+    int32_t countsLeftS[360];
+    int32_t countsDownLeftS[360];
+    int32_t countsDownRightS[360];
+    int32_t countsRightS[360];
+    int32_t countsUpRightS[360];
     float avgAsymXS;
     float avgAsymX45S;
     float avgAsymX90S;
@@ -97,33 +99,36 @@ typedef struct {
     float beamEnergyS;
     float analyzingPowerS;
     float analyzingPowerErrorS;
-    long numberEventsS;
-    long maxTimeS;
+    int32_t numberEventsS;
+    int32_t maxTimeS;
 } polDataStruct;
 
 typedef struct {
-    long movingM;
-    long positionEncM[2];
+    int32_t movingM;
+    int32_t positionEncM[2];
     char statusM[80];
     char positionS[80];
 } targetDataStruct;
 
 
 typedef struct{
-    long VertLinear ; // vertical target linear encorder
-    long VertRotary ; // vertical target rotary encorder
-    long HorLinear  ; // horizontal target linear encorder
-    long HorRotary  ; // horizontal target linear encorder
+    int32_t VertLinear ; // vertical target linear encorder
+    int32_t VertRotary ; // vertical target rotary encorder
+    int32_t HorLinear  ; // horizontal target linear encorder
+    int32_t HorRotary  ; // horizontal target linear encorder
 } pCTargetStruct;
 
+#pragma pack(push)
+#pragma pack(1)
 typedef struct {
     double    beamEnergyM;
-    long      fillNumberM;
-    short int measuredFillPatternM[360];
+    int32_t   fillNumberM;
+    int16_t   measuredFillPatternM[360];
     float     bunchLengthS;                  // half bunch length?
     float     synchrotronTuneS;
-    short int polarizationFillPatternS[360];
+    int16_t   polarizationFillPatternS[360];
 } beamDataStruct;
+#pragma pack(pop)
 
 typedef struct {
     float fillDataM[360];
@@ -131,18 +136,18 @@ typedef struct {
 } wcmDataStruct;
 
 typedef struct {
-    int jetAbsolutePosition;
-    int rbpm[8];
+    int32_t jetAbsolutePosition;
+    int32_t rbpm[8];
 } jetPositionStruct;
 
 typedef struct {
-    unsigned short positionDelay;
-    unsigned short flags;               // what parameters to be send to CDEV: 0x8000 - positionDelay, 8 ls-bits individual channels
+    uint16_t positionDelay;
+    uint16_t flags;               // what parameters to be send to CDEV: 0x8000 - positionDelay, 8 ls-bits individual channels
     struct V124CHAN {
-        unsigned short fineDelay;
-        unsigned short pulseWidth;
-        unsigned short bucketOffset;
-        unsigned short bucketPeriod;
+        uint16_t fineDelay;
+        uint16_t pulseWidth;
+        uint16_t bucketOffset;
+        uint16_t bucketPeriod;
     } chan[8];
 } V124Struct;
 
@@ -165,8 +170,8 @@ typedef struct {
 #define JET_MINUS       0x0004
 
 typedef struct {
-    short int CrateN;   // crate number
-    short int CamacN;   // station number
+    int16_t CrateN;     // crate number
+    int16_t CamacN;     // station number
     int VirtexN;        // Virtex number
     union {
         struct {
@@ -174,7 +179,7 @@ typedef struct {
             unsigned char End;
             unsigned short dummy;
         } split;
-        long reg;
+        int32_t reg;
     } Window;           // Better to have it individually
     short unsigned int LookUp[512];
     float ETCutW;       // +- nanoseconds
@@ -194,7 +199,7 @@ typedef struct {
     float pede;         // pedestal of unknown nature (unitS ?)
     float C[5];         // nonlinear fit parameters: Ein = C[0] + C[1]*<ADC> + C[2]*<ADC>^2 ...
     float TOFLength;    // cm. 2010: we now have it different per detector !
-    long reserved[9];
+    int32_t reserved[9];
 } SiChanStruct;
 
 #define CALIB_CONSTANTS 13
@@ -208,53 +213,53 @@ typedef struct {
 
 
 typedef struct {
-    short int CrSc;             // scaler module Crate
-    short int NSc;              // scaler module N
-    short int CrOut;            // output module Crate
-    short int NOut;             // output module N
-    short int CrIn;             // input  module Crate
-    short int NIn;              // input  module N
-    short int CrLam;            // LAM    module Crate
-    short int NLam;             // LAM    module N
-    int DelimPrescaler;         // 78 KHz are prescaled with this number to make the delimeter
+    int16_t CrSc;               // scaler module Crate
+    int16_t NSc;                // scaler module N
+    int16_t CrOut;              // output module Crate
+    int16_t NOut;               // output module N
+    int16_t CrIn;               // input  module Crate
+    int16_t NIn;                // input  module N
+    int16_t CrLam;              // LAM    module Crate
+    int16_t NLam;               // LAM    module N
+    int32_t DelimPrescaler;     // 78 KHz are prescaled with this number to make the delimeter
     float WFDTUnit;             // 1/6 70 MHz period, ns, 2*LSB time measurement
     float TOFLength;            // cm. 2010: Obsolete - moved to individual channels
     float AtomicNumber;         // just atomic number of the recoiled particle (carbon or proton)
     union {
         struct {
-            unsigned Mode:2;    // [1:0] 0-Raw, 1-SubMean, 2-AT, 3-ReadAll
-            unsigned iDiv:2;    // [3:2] Integral divider parameter
+            uint32_t Mode:2;    // [1:0] 0-Raw, 1-SubMean, 2-AT, 3-ReadAll
+            uint32_t iDiv:2;    // [3:2] Integral divider parameter
                                 // Int=IntRead*2^(iDiv+2)
-            unsigned MemEn:1;   // [4] Output to memory enabled if 1
-            unsigned Mod2D:1;   // [5] 0-coarse, 1-fine (in t)
-            unsigned VirtN:2;   // [7:6] Channel number to appear in data stream to memory
-            unsigned Filter:1;  // [8] Enable 3pt filter, CFD threshold for v.10
-            unsigned MaxCor:1;  // [9] Enable maximum correction (no effect unless the tables are programmed)
-            unsigned B120:1;    // [10] Enable 120 bunch mode
-            unsigned G140:1;    // [11] Dummy in this design, enable trig. window for jet mode v. 10
-            unsigned RMemLUp:1; // [12] Selection of events to memory: 0-regular
+            uint32_t MemEn:1;   // [4] Output to memory enabled if 1
+            uint32_t Mod2D:1;   // [5] 0-coarse, 1-fine (in t)
+            uint32_t VirtN:2;   // [7:6] Channel number to appear in data stream to memory
+            uint32_t Filter:1;  // [8] Enable 3pt filter, CFD threshold for v.10
+            uint32_t MaxCor:1;  // [9] Enable maximum correction (no effect unless the tables are programmed)
+            uint32_t B120:1;    // [10] Enable 120 bunch mode
+            uint32_t G140:1;    // [11] Dummy in this design, enable trig. window for jet mode v. 10
+            uint32_t RMemLUp:1; // [12] Selection of events to memory: 0-regular
                                 // lookup, 1-rectangular lookup (ampl thresh=FineHistBeg,
                                 // time thresh=Window.Beg)
-            unsigned RevDelim:1;// [13] = 0 for external, = 1 for revolution triggered delimeter
-            unsigned Hi2MemA:1; // [14] (RO) Histograms to memory procedure in progress
-            unsigned Delim:1;   // [15] (RO) Delimeter active
-            unsigned OvF:1;     // [16] (RO) FIFO Overflow
-            unsigned ScClr:1;   // [17] (RO) Scaler/Histogram clear in progress
-            unsigned IntOvF:1;  // [18] (RO) Integral dvided by 2^(iDiv+2) >255
-            unsigned ScOvF:1;   // [19] (RO) Scalers/histograms overflow (counters wrap)
-            unsigned dummy:3;
-            unsigned DLLNLock:1; // [23] (RO) DLL not locked
+            uint32_t RevDelim:1;// [13] = 0 for external, = 1 for revolution triggered delimeter
+            uint32_t Hi2MemA:1; // [14] (RO) Histograms to memory procedure in progress
+            uint32_t Delim:1;   // [15] (RO) Delimeter active
+            uint32_t OvF:1;     // [16] (RO) FIFO Overflow
+            uint32_t ScClr:1;   // [17] (RO) Scaler/Histogram clear in progress
+            uint32_t IntOvF:1;  // [18] (RO) Integral dvided by 2^(iDiv+2) >255
+            uint32_t ScOvF:1;   // [19] (RO) Scalers/histograms overflow (counters wrap)
+            uint32_t dummy:3;
+            uint32_t DLLNLock:1; // [23] (RO) DLL not locked
         } split;
-        long WFDMode;
-        long reg;
+        int32_t WFDMode;
+        int32_t reg;
     } CSR;
     union {
         struct {
-            unsigned char TrigThreshold; // In ampl channels, thresh. for baseline subtraction
-            unsigned char FineHistBeg;   // In ampl channels, LL of A in AThist in fine mode
-            unsigned short dummy;
+            uint8_t TrigThreshold; // In ampl channels, thresh. for baseline subtraction
+            uint8_t FineHistBeg;   // In ampl channels, LL of A in AThist in fine mode
+            uint16_t dummy;
         } split;
-        long reg;
+        int32_t reg;
     } TRG;
     char Pattern[120];      // Pattern: this is just for some crosscheck
                             // 0 - not filled;
@@ -264,21 +269,21 @@ typedef struct {
 //
     float Emin;         // LookUp table threshold, KeV
     float Emax;         // LookUp table Maximum, KeV
-    int ETLookUp;       // How to fill lookup tables: 0-banana, 1-rectangle,
+    int32_t ETLookUp;   // How to fill lookup tables: 0-banana, 1-rectangle,
                         // N>1-threshold=N in WFD ampl units
-    int IALookUp;       // 0-linear, 1-fully open
-    int BZDelay;        // delay of BZ pulse in 70 MHz clocks (1-14)
+    int32_t IALookUp;   // 0-linear, 1-fully open
+    int32_t BZDelay;    // delay of BZ pulse in 70 MHz clocks (1-14)
                         // Bzdelay is not necessary at RHIC, but may be usefull
-    int OnlyHist;       // If 1, no data is read out, only internal histograms
+    int32_t OnlyHist;   // If 1, no data is read out, only internal histograms
     float SleepFraction; // [0..1] - fraction of time to set inhibit to optimize memory usage
     float CicleTime;    // time period to test jet Veto
     float TrigMin;      // trigger level for internal 2-dim histogram and for rectangular cut for memory
-    int JetDelay;       // Value to be written into delay register of version 10
-    int FPGAVersion;    // FPGA version. We don't support various versions in one run
+    int32_t JetDelay;   // Value to be written into delay register of version 10
+    int32_t FPGAVersion;// FPGA version. We don't support various versions in one run
     float TshiftLow;    // tshift for injection
     float TshiftHigh;   // tshift for flattop
-    int Dummy[33];      // For futher use to keep the whole length the same
-    int NumChannels;            // number of silicon strips
+    int32_t Dummy[33];  // For futher use to keep the whole length the same
+    int32_t NumChannels; // number of silicon strips
     SiChanStruct chan[1];       // array of NumChannels size
 } configRhicDataStruct;
 
@@ -294,7 +299,7 @@ STATIC_ASSERT(sizeof(wcmDataStruct) == 4*360 + 4, wcmDataStruct_size)
 STATIC_ASSERT(sizeof(jetPositionStruct) == 4*(1+8), jetPositionStruct_size)
 STATIC_ASSERT(sizeof(V124Struct) == 2*2 + 2*4*8, V124Struct_size)
 STATIC_ASSERT(sizeof(SiChanStruct) == 2*2 + 4 + 4 + 2*512 + 4*(10+5+1) + 4*9, SiChanStruct_size)
-#define CRDS_PTR ((configRhicDataStruct*)NULL)
+#define CRDS_PTR ((configRhicDataStruct*)0)
 STATIC_ASSERT(sizeof(CRDS_PTR->CSR) == 4, configRhicDataStruct_CSR_size)
 STATIC_ASSERT(sizeof(CRDS_PTR->TRG) == 4, configRhicDataStruct_TRG_size)
 STATIC_ASSERT(sizeof(configRhicDataStruct) == 2*8 + 4 + 4*3 + sizeof(CRDS_PTR->CSR) + sizeof(CRDS_PTR->TRG) + 120 + 4*2 + 4*4 + 4*3 + 4*2 + 4*2 + 4*(33+1) + sizeof(SiChanStruct), configRhicDataStruct_size)
