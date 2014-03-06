a0d675a7204b9d013de0350211980422b9bddb92
Dmitry Kalinkin <dkalinkin@bnl.gov>
2014-03-05 23:38:04 -0500

    malpha: don't fit whole run13, make it only for beamtime

--- src/malpha.cxx
+++ src/malpha.cxx
@@ -318,8 +318,26 @@ void PlotMean(DrawObjContainer *oc, const string &polIdName, const char *name, R
          fit_daily.SetLineColor(line_color);
          det_fit_daily.SetLineColor(kBlack);
          det_fit_daily.SetLineWidth(1);
-         TFitResultPtr fitres = g->Fit(&fit_daily, "QS"); // Q: quiet, S: return fitres
-         det_g->Fit(&det_fit_daily, "Q"); // Q: quiet
+
+         TFitResultPtr fitres;
+         {
+            time_t t = max_startTime;
+            struct tm *time = gmtime(&t);
+            if (1900+time->tm_year == 2013)
+            {
+               Warning("malpha", "detected run13 : will fit only during beamtime");
+               const double fit_min = 1362096000 - min_startTime; // 03/01/2013 00:00:00
+               const double fit_max = 1372636800 - min_startTime; // 07/01/2013 00:00:00
+               fitres = g->Fit(&fit_daily, "QS", "", fit_min, fit_max); // Q: quiet, S: return fitres
+               det_g->Fit(&det_fit_daily, "Q", "", fit_min, fit_max);   // Q: quiet
+            }
+            else
+            {
+               fitres = g->Fit(&fit_daily, "QS"); // Q: quiet, S: return fitres
+               det_g->Fit(&det_fit_daily, "Q");   // Q: quiet
+            }
+         }
+
          char buf[256];
          if (fitres.Get())
          {
