c3c4c1d0b0d650c9558aac92a47181a58afefe8b
Dmitri Smirnov <d.s@plexoos.com>
2014-11-25 09:54:25 -0500

    AsymCalculator: Removed commented out code

--- inc/AsymCalculator.h
+++ inc/AsymCalculator.h
@@ -47,7 +47,6 @@ public:
    static DetLRSet X45BDets; //!
    static DetLRSet Y45Dets; //!
 
-   //void          CalcBunchAsym(TH2 &hDetVsBunchId);
    void          CalcBunchAsym(DrawObjContainer *oc);
    void          CalcBunchAsymSqrtFormula(DrawObjContainer *oc);
    void          CalcDelimAsym(DrawObjContainer *oc);
@@ -75,7 +74,6 @@ public:
    void  CalcLongiChAsym(DrawObjContainer *oc);
    void  CalcStripAsymmetry(int Mode);
    TH1D* CalcChannelAsym(TH1I &hUp, TH1I &hDown, TH1D* hChAsym=0);
-   //void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag);
 
    TFitResultPtr FitChAsymConst(TH1D &hChAsym, TGraphErrors *gr=0);
    TFitResultPtr FitChAsymSine(TH1D &hChAsym, TGraphErrors *gr=0);
@@ -90,11 +88,6 @@ public:
    static ValErrPair CalcAsym(Double_t A, Double_t B, Double_t totalA, Double_t totalB);
    static ValErrPair CalcAsymSqrtFormula(Double_t A, Double_t B, Double_t C, Double_t D);
    static void       sqass(Double_t A, Double_t B, Double_t C, Double_t D, Double_t &asym, Double_t &asymErr);
-
-   // following 3 subroutines are unsuccessful MINUIT sin(phi) fit routines.
-   //void     SinPhiFit(Float_t p0, Float_t *P, Float_t *phi, Float_t &chi2);
-   //Float_t  sinx(Float_t, Double_t *);
-   //Double_t GetFittingErrors(TMinuit *gMinuit, Int_t NUM);
 };
 
 
