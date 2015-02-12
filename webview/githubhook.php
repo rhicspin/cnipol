e7d7d2a6def88b8df1cf678da896cdc9e3f71c1e
Dmitry Kalinkin <dkalinkin@bnl.gov>
2015-02-11 23:35:52 -0500

    githubhook: update
    1) set up-to-date webhook ip whitelist as in https://help.github.com/articles/what-ip-addresses-does-github-use-that-i-should-whitelist/
    2) doxygen cmd for current setup

--- webview/githubhook.php
+++ webview/githubhook.php
@@ -1,15 +1,14 @@
 <?php
-// The Public IP addresses for these hooks are: 204.232.175.64/27, 192.30.252.0/22.
+// The Public IP addresses for these hooks are in 192.30.252.0/22.
 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $ip = array_pop(explode(", ", $ip));
 $ip = ip2long($ip);
-$mask_27 = 0xFFFFFFE0;
 $mask_22 = 0xFFFFFC00;
-if ((($ip & $mask_27) === ip2long('204.232.175.64')) || (($ip & $mask_22) === ip2long('192.30.252.0')))
+if (($ip & $mask_22) === ip2long('192.30.252.0'))
 {
 	$payload = $_POST['payload'];
 	if (!$payload) die('no');
-	system('git fetch 1>/dev/null 2>/dev/null; git stash 1>/dev/null; git checkout origin/master; cd /home/cnipol/cnipol_web_production/doc; /usr/local/bin/doxygen 1>/dev/null 2>/dev/null');
+	system('git fetch 1>/dev/null 2>/dev/null; git stash 1>/dev/null; git checkout origin/master; cd ../doc; doxygen 1>/dev/null 2>/dev/null');
 	echo 'ok';
 }
 else
