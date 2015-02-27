rhicspin/cnipol

commit 05b17a548729fc79c307c0c121861fe21e2824b0

MeasInfo: run 15 detection
Comparison of config/crate?.ini files between run13 and run15 suggests
that WFD mapping has changed.

src/MeasInfo.cxx
@@ -502,9 +502,12 @@ void MeasInfo::Update(MseMeasInfoX& run)
       fRunId = 11;
    } else if (RUNID >= 16000 && RUNID < 16800) { // Run 12
       fRunId = 12;
-   } else if (RUNID >= 17000 ) { // Run 13
+   } else if (RUNID >= 17000 && RUNID < 18000) { // Run 13
       fRunId = 13;
       gCh2WfdMap = ch2WfdMap_run13;
+   } else if (RUNID >= 18000) {
+      fRunId = 15;
+      // WFD mapping has changed
    } else {
       // default Run value
    }

commit b427f9e3272740cd6cc51fa5826eebcfce13dd31

BUGFIX: Put WFD mapping back
Turns out WFD mapping hasn't really changed in run15.

This fixes bug introduced in 05b17a548729fc79c307c0c121861fe21e2824b0

src/MeasInfo.cxx
@@ -507,7 +507,7 @@ void MeasInfo::Update(MseMeasInfoX& run)
       gCh2WfdMap = ch2WfdMap_run13;
    } else if (RUNID >= 18000) {
       fRunId = 15;
-      // WFD mapping has changed
+      gCh2WfdMap = ch2WfdMap_run13;
    } else {
       // default Run value
    }